4198aafe787355e4ecbba923ccbd8a459c286eaf
wehrens <wehrens@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2002-08-28 15:24:00 +0000

    Added methods PortletDeploymentDescriptor
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@22 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

--- src/org/gridlab/gridsphere/portletcontainer/descriptor/PortletDeploymentDescriptor.java
+++ src/org/gridlab/gridsphere/portletcontainer/descriptor/PortletDeploymentDescriptor.java
@@ -1,26 +1,88 @@
 /*
  * @author <a href="mailto:novotny@aei.mpg.de">Jason Novotny</a>
+ * @author <a href="mailto:oliver@wehrens.de">Oliver Wehrens</a>
  * @version $Id$
  */
 package org.gridlab.gridsphere.portletcontainer.descriptor;
 
+import org.gridlab.gridsphere.core.persistence.ConfigurationException;
+import org.gridlab.gridsphere.core.persistence.RestoreException;
+import org.gridlab.gridsphere.core.persistence.castor.PersistenceManagerXml;
+
 import java.util.Iterator;
+import java.util.Vector;
 
 public class PortletDeploymentDescriptor {
 
-    public PortletDeploymentDescriptor() {
-        load();
-    }
-    public PortletApp[] getPortletApp();
 
+    static org.apache.log4j.Category cat = org.apache.log4j.Category.getInstance(PortletDeploymentDescriptor.class.getName());
+
+    Vector PortletApp;
+
+    /**
+     *  construcs PortletDeploymentDescriptor
+     * @param url localtion of the portlet.xml
+     * @param mapping localtion of the mapping file
+     */
+    public PortletDeploymentDescriptor(String url, String mapping) throws PortletDeploymentDescriptorException  {
+        load(url, mapping);
+    }
+
+    /**
+     *  returns a vector of portletapps
+     */
+    public Vector getPortletApp() {
+        return PortletApp;
+    };
+
+    /**
+     * sets the PortletApp Vector
+     */
+    private void setPortletApp(Vector v) {
+        PortletApp = v;
+    }
+
+    /**
+     * creates a new PortletApp (which is a vector)
+     */
     public PortletApp createPortletApp() {
         return new PortletApp();
     }
 
+    /**
+     *  <b>not implemented yet</b>
+     */
     public void addPortletApp();
 
-    public void load();
 
+    public void load(String url, String mapping) throws PortletDeploymentDescriptorException  {
+        PersistenceManagerXml pmx = new PersistenceManagerXml();
+
+        // where is the portlet.xml ?
+        pmx.setConnectionURL(url);
+
+        // set the path to the mapping file
+        pmx.setMappingFile(mapping);
+
+        PortletDefinition pd = null;
+
+        // try to get it
+        try {
+             pd = (PortletDefinition)pmx.restoreObject();
+        } catch (RestoreException e) {
+            cat.error("RestoreError "+e);
+            throws new PortletDeploymentDescriptorException("Unable to restore: "+e);
+        } catch (ConfigurationException e) {
+            cat.error("ConfigurationError "+e);
+            throws new PortletDeploymentDescriptorException("Configuration error: "+e);
+        }
+
+        this.setPortletApp(pd.getPortletAppList());
+    }
+
+    /**
+     * <b>not implemented yet</b>
+     */
     public void save();
 
 }
