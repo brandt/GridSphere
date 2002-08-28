bf6496604d98867a11de494336ea245161f26c6e
wehrens <wehrens@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2002-08-28 16:05:23 +0000

    Fixed compileerrors
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@26 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

--- src/org/gridlab/gridsphere/portletcontainer/descriptor/PortletDeploymentDescriptor.java
+++ src/org/gridlab/gridsphere/portletcontainer/descriptor/PortletDeploymentDescriptor.java
@@ -17,10 +17,10 @@ public class PortletDeploymentDescriptor {
 
     static org.apache.log4j.Category cat = org.apache.log4j.Category.getInstance(PortletDeploymentDescriptor.class.getName());
 
-    Vector PortletApp;
+    private Vector PortletApp;
 
     /**
-     *  construcs PortletDeploymentDescriptor
+     * construcs PortletDeploymentDescriptor
      * @param url localtion of the portlet.xml
      * @param mapping localtion of the mapping file
      */
@@ -45,14 +45,14 @@ public class PortletDeploymentDescriptor {
     /**
      * creates a new PortletApp (which is a vector)
      */
-    public PortletApp createPortletApp() {
+  /*  public PortletApp createPortletApp() {
         return new PortletApp();
     }
-
+  */
     /**
      *  <b>not implemented yet</b>
      */
-    public void addPortletApp();
+    public void addPortletApp() {};
 
 
     public void load(String url, String mapping) throws PortletDeploymentDescriptorException  {
@@ -70,11 +70,11 @@ public class PortletDeploymentDescriptor {
         try {
              pd = (PortletDefinition)pmx.restoreObject();
         } catch (RestoreException e) {
-            cat.error("RestoreError "+e);
-            throws new PortletDeploymentDescriptorException("Unable to restore: "+e);
+            cat.error("RestoreError "+e.getMessage());
+            throw new PortletDeploymentDescriptorException("Unable to restore: "+e.getMessage());
         } catch (ConfigurationException e) {
             cat.error("ConfigurationError "+e);
-            throws new PortletDeploymentDescriptorException("Configuration error: "+e);
+            throw new PortletDeploymentDescriptorException("Configuration error: "+e.getMessage());
         }
 
         this.setPortletApp(pd.getPortletAppList());
@@ -83,6 +83,6 @@ public class PortletDeploymentDescriptor {
     /**
      * <b>not implemented yet</b>
      */
-    public void save();
+    public void save() {};
 
 }
