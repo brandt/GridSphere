092e7f202affc4500cf1d285f578569fc0c59ea9
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2002-09-03 19:15:50 +0000

    added helper method to PortletServiceFactory so the properties file is taken from the ServletContext.getInitParam so one doesn't need to pass in a properties file. More robust class instantiation in factoryImpl
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@90 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

--- src/org/gridlab/gridsphere/portlet/service/spi/PortletServiceFactory.java
+++ src/org/gridlab/gridsphere/portlet/service/spi/PortletServiceFactory.java
@@ -5,8 +5,8 @@
 package org.gridlab.gridsphere.portlet.service.spi;
 
 import org.gridlab.gridsphere.portlet.service.PortletService;
-import org.gridlab.gridsphere.portlet.service.PortletServiceUnavailableException;
 import org.gridlab.gridsphere.portlet.service.PortletServiceNotFoundException;
+import org.gridlab.gridsphere.portlet.service.PortletServiceUnavailableException;
 
 import javax.servlet.ServletConfig;
 import java.util.Properties;
@@ -16,6 +16,21 @@ import java.util.Properties;
  * The PortletServiceFactory interface has to be implemented by all portlet service factory implementations.
  */
 public interface PortletServiceFactory {
+
+    /**
+     * createPortletServiceFactory instantiates the given class and initializes it
+     *
+     * @param service the class of the service
+     * @param servletConfig the servlet configuration
+     * @param boolean reuse a previous initialized service if true, otherwise create a new service instance if false
+     * @return the instantiated portlet service
+     * @throws PortletServiceUnavailableException if the portlet service is unavailable
+     * @throws PortletServiceNotFoundException if the PortletService is not found
+     */
+    public PortletService createPortletService(Class service,
+                                               ServletConfig servletConfig,
+                                               boolean useCachedService)
+            throws PortletServiceUnavailableException, PortletServiceNotFoundException;
 
     /**
      * createPortletServiceFactory instantiates the given class and initializes it
@@ -32,6 +47,6 @@ public interface PortletServiceFactory {
                                                Properties serviceProperties,
                                                ServletConfig servletConfig,
                                                boolean useCachedService)
-                                        throws PortletServiceUnavailableException, PortletServiceNotFoundException;
+            throws PortletServiceUnavailableException, PortletServiceNotFoundException;
 
 }
--- src/org/gridlab/gridsphere/portlet/service/spi/impl/SportletServiceFactory.java
+++ src/org/gridlab/gridsphere/portlet/service/spi/impl/SportletServiceFactory.java
@@ -4,19 +4,21 @@
  */
 package org.gridlab.gridsphere.portlet.service.spi.impl;
 
+import org.gridlab.gridsphere.portlet.PortletLog;
 import org.gridlab.gridsphere.portlet.service.PortletService;
-import org.gridlab.gridsphere.portlet.service.PortletServiceUnavailableException;
 import org.gridlab.gridsphere.portlet.service.PortletServiceNotFoundException;
-import org.gridlab.gridsphere.portlet.service.spi.impl.SportletServiceConfig;
+import org.gridlab.gridsphere.portlet.service.PortletServiceUnavailableException;
+import org.gridlab.gridsphere.portlet.service.spi.PortletServiceConfig;
 import org.gridlab.gridsphere.portlet.service.spi.PortletServiceFactory;
 import org.gridlab.gridsphere.portlet.service.spi.PortletServiceProvider;
-import org.gridlab.gridsphere.portlet.service.spi.PortletServiceConfig;
-import org.gridlab.gridsphere.portlet.PortletLog;
-import org.gridlab.gridsphere.portlet.User;
 
 import javax.servlet.ServletConfig;
-import java.util.*;
-import java.lang.reflect.Constructor;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  * The PortletServiceCacheFactory provides a singleton factory to create portlet services.
@@ -43,7 +45,8 @@ public class SportletServiceFactory implements PortletServiceFactory {
     /**
      * doSync is used to ensure only one thread has created an instance
      */
-    private synchronized static void doSync() {}
+    private synchronized static void doSync() {
+    }
 
     public static SportletServiceFactory getInstance() {
         if (instance == null) {
@@ -51,6 +54,38 @@ public class SportletServiceFactory implements PortletServiceFactory {
             instance = new SportletServiceFactory();
         }
         return instance;
+    }
+
+    /**
+     * createPortletServiceFactory instantiates the given class and initializes it
+     *
+     * @param service the class of the service
+     * @param servletConfig the servlet configuration
+     * @param boolean reuse a previous initialized service if true, otherwise create a new service instance if false
+     * @return the instantiated portlet service
+     * @throws PortletServiceUnavailableException if the portlet service is unavailable
+     * @throws PortletServiceNotFoundException if the PortletService is not found
+     */
+    public PortletService createPortletService(Class service,
+                                               ServletConfig servletConfig,
+                                               boolean useCachedService)
+            throws PortletServiceUnavailableException, PortletServiceNotFoundException {
+
+        Properties serviceProperties = new Properties();
+        String appRoot = servletConfig.getServletContext().getRealPath("");
+        String serviceProps = servletConfig.getInitParameter("PortletServices.properties");
+        String propsPath = appRoot + "/" + serviceProps;
+        FileInputStream fistream = null;
+        try {
+            fistream = new FileInputStream(propsPath);
+            serviceProperties.load(fistream);
+        } catch (IOException e) {
+            log.error("Unable to find properties file: " + propsPath);
+            log.error("Make sure location is specified in server.xml " + propsPath);
+            throw new PortletServiceUnavailableException("Unable to find/load properties file: " + propsPath);
+        }
+        return createPortletService(service, serviceProperties, servletConfig, useCachedService);
+
     }
 
     /**
@@ -73,14 +108,23 @@ public class SportletServiceFactory implements PortletServiceFactory {
         PortletServiceProvider psp = null;
 
         // see if we already have an instance of this service
+        if (service == null) {
+            throw new PortletServiceUnavailableException("Received null service class");
+        }
         if (useCachedService) {
             psp = (PortletServiceProvider)services.get(service);
             if (psp != null) return psp;
         }
         String serviceName = service.getName();
-        String serviceImpl = serviceProperties.getProperty(serviceName).trim();
+
+        if (serviceProperties == null) {
+            log.error("Received null service properties");
+            throw new PortletServiceUnavailableException("Received null service properties");
+        }
+        String serviceImpl = serviceProperties.getProperty(serviceName);
         if (serviceImpl == null) {
-            log.error("Unable to find implementing portlet service: " + serviceName);
+            log.error("Unable to find implementing portlet service: " + serviceName +
+                    " . Please check PortletServices.properties file for proper service entry");
             throw new PortletServiceNotFoundException("Unable to find implementing portlet service: " + serviceName);
         }
         PortletServiceConfig portletServiceConfig =
@@ -108,7 +152,7 @@ public class SportletServiceFactory implements PortletServiceFactory {
     public void shutdownService(Class service) {
         if (services.containsKey(service)) {
             log.info("Shutting down service: " + service.getName());
-            PortletServiceProvider psp = (PortletServiceProvider)services.get(service);
+            PortletServiceProvider psp = (PortletServiceProvider) services.get(service);
             psp.destroy();
         }
     }
@@ -121,8 +165,8 @@ public class SportletServiceFactory implements PortletServiceFactory {
         Enumeration keys = services.keys();
         log.info("Shutting down all services:");
         while (keys.hasMoreElements()) {
-            Class service = (Class)keys.nextElement();
-            PortletServiceProvider psp = (PortletServiceProvider)services.get(service);
+            Class service = (Class) keys.nextElement();
+            PortletServiceProvider psp = (PortletServiceProvider) services.get(service);
             log.info("Shutting down service: " + service.getName() + " impl: " + psp.getClass().getName());
             psp.destroy();
         }
