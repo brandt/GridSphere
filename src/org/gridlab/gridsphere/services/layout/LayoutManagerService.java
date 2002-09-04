487e2771564cc7e222aae5ff0a733683a05047b2
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2002-09-04 15:56:43 +0000

    nothing yet
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@101 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/services/layout/LayoutManagerService.java
@@ -0,0 +1,17 @@
+/*
+ * @author <a href="mailto:novotny@aei.mpg.de">Jason Novotny</a>
+ * @version $Id$
+ */
+package org.gridlab.gridsphere.services.layout;
+
+import org.gridlab.gridsphere.portlet.service.PortletService;
+
+
+/**
+ * The LayoutManagerService
+ */
+public interface LayoutManagerService extends PortletService {
+
+
+
+}
