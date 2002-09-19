289363995f5a43e70907667ceb65d82a83916751
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2002-09-19 14:52:16 +0000

    master test class
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@179 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ tests/org/gridlab/gridsphere/AllJUnitTests.java
@@ -0,0 +1,31 @@
+/*
+ * @author <a href="mailto:novotny@aei.mpg.de">Jason Novotny</a>
+ * @version $Id$
+ */
+package org.gridlab.gridsphere;
+
+import junit.framework.TestSuite;
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+import org.gridlab.gridsphere.services.security.acl.ACLServiceTest;
+import org.gridlab.gridsphere.services.user.UserManagerServiceTest;
+import org.gridlab.gridsphere.portletcontainer.descriptor.DescriptorTest;
+
+/**
+ * Simple class to build a TestSuite out of the individual test classes.
+ */
+public class AllJUnitTests extends TestCase {
+
+    public AllJUnitTests(String name) {
+        super(name);
+    }
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite(ACLServiceTest.class);
+        suite.addTest(new TestSuite(UserManagerServiceTest.class));
+        suite.addTest(new TestSuite(DescriptorTest.class));
+        return suite;
+   }
+}
+
