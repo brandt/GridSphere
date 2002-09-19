434dd69e2a9344c934ba26951e83f8f41beb3913
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2002-09-19 14:52:36 +0000

    moved this over
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@180 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ tests/org/gridlab/gridsphere/portlet/service/spi/ServiceTest.java
@@ -0,0 +1,61 @@
+/*
+ * @author <a href="mailto:novotny@aei.mpg.de">Jason Novotny</a>
+ * @version $Id$
+ */
+package org.gridlab.gridsphere.portlet.service.spi;
+
+import junit.framework.TestCase;
+import org.gridlab.gridsphere.portlet.service.spi.impl.SportletServiceFactory;
+import org.gridlab.gridsphere.portlet.service.PortletServiceException;
+import org.gridlab.gridsphere.portlet.impl.SportletLog;
+import org.gridlab.gridsphere.portlet.PortletLog;
+import org.apache.log4j.PropertyConfigurator;
+
+import java.util.Properties;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.File;
+
+/**
+ * This is the base fixture for service testing. Provides a service factory and the
+ * properties file.
+ */
+public class ServiceTest extends TestCase {
+
+    protected static SportletServiceFactory factory = null;
+    protected static Properties props = null;
+    private static PortletLog log = SportletLog.getInstance(ServiceTest.class);
+
+
+    public ServiceTest(String name) {
+        super(name);
+    }
+
+    protected void setUp() {
+
+        PropertyConfigurator.configure("conf/log4j.properties");
+
+        // create factory
+        factory = SportletServiceFactory.getInstance();
+
+        // create properties
+        props = new Properties();
+        FileInputStream fistream = null;
+        String fullPath = System.getProperty("user.dir") + "/webapps/WEB-INF/conf/PortletServices.properties";
+        log.info("loading properties file: " + fullPath);
+        try {
+            fistream = new FileInputStream(fullPath);
+            props.load(fistream);
+        } catch (IOException e) {
+            log.error("Unable to load properties file", e);
+        }
+
+    }
+
+    protected void tearDown() {
+        SportletServiceFactory.getInstance().shutdownServices();
+    }
+}
