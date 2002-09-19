5137c16da4b348f2075824d50cd19159dda835c5
wehrens <wehrens@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2002-09-19 10:27:20 +0000

    Object for group acl
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@171 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/services/security/acl/impl2/Groups.java
@@ -0,0 +1,40 @@
+/*
+ * @author <a href="mailto:oliver@wehrens.de">Oliver Wehrens</a>
+ * @team sonicteam
+ * @version $Id$
+ */
+
+package org.gridlab.gridsphere.services.security.acl.impl2;
+
+import org.gridlab.gridsphere.core.persistence.BaseObject;
+import org.gridlab.gridsphere.portlet.PortletGroup;
+
+public class Groups extends BaseObject implements PortletGroup {
+    static org.apache.log4j.Category cat = org.apache.log4j.Category.getInstance(Groups.class.getName());
+
+    private String Name = "";
+    private int ID = 0;
+
+    public Groups() {
+        super();
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getID() {
+        return this.ID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+}
+
+
