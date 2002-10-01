b25886d3279a3108488f0fb3e0d5ecb0d5e96338
wehrens <wehrens@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2002-10-01 15:10:04 +0000

    added the castor jdo persistence interface
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@235 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

--- src/org/gridlab/gridsphere/core/persistence/BaseObject.java
+++ src/org/gridlab/gridsphere/core/persistence/BaseObject.java
@@ -9,7 +9,9 @@
 
 package org.gridlab.gridsphere.core.persistence;
 
-public class BaseObject implements org.exolab.castor.jdo.TimeStampable {
+import org.exolab.castor.jdo.Database;
+
+public class BaseObject implements org.exolab.castor.jdo.TimeStampable, org.exolab.castor.jdo.Persistent {
 
     static org.apache.log4j.Category cat = org.apache.log4j.Category.getInstance(BaseObject.class.getName());
 
@@ -58,5 +60,34 @@ public class BaseObject implements org.exolab.castor.jdo.TimeStampable {
     public void setOid(String oid) {
         Oid = oid;
     }
+
+    public void jdoPersistent(Database database) {
+    }
+
+    public void jdoTransient() {
+    }
+
+    public Class jdoLoad(short i) throws Exception {
+        return null;
+    }
+
+    public void jdoStore(boolean b) throws Exception {
+    }
+
+    public void jdoBeforeCreate(Database database) throws Exception {
+    }
+
+    public void jdoAfterCreate() throws Exception {
+    }
+
+    public void jdoBeforeRemove() throws Exception {
+    }
+
+    public void jdoAfterRemove() throws Exception {
+    }
+
+    public void jdoUpdate() throws Exception {
+    }
 }
+
 
