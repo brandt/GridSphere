0341e32909ab8342b0bb13ab83dcf08d4d3992dc
wehrens <wehrens@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2002-10-09 13:02:37 +0000

    stupid classes for saving stuff
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@267 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/services/user/impl/AccountRequestImplDesiredGroups.java
@@ -0,0 +1,14 @@
+/*
+ * @author <a href="mailto:oliver@wehrens.de">Oliver Wehrens</a>
+ * @team sonicteam
+ * @version $Id$
+ */
+
+package org.gridlab.gridsphere.services.user.impl;
+
+import org.gridlab.gridsphere.core.persistence.castor.StringVector;
+
+public class AccountRequestImplDesiredGroups extends StringVector {
+
+}
+
