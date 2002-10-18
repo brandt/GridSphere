4812d5fc291bb524004cd1efd694410d76dae536
wehrens <wehrens@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2002-10-18 12:44:11 +0000

    Unique ID class
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@326 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/core/persistence/UniqueID.java
@@ -0,0 +1,21 @@
+/*
+ * @author <a href="mailto:oliver@wehrens.de">Oliver Wehrens</a>
+ * @team sonicteam
+ * @version $Id$
+ */
+
+package org.gridlab.gridsphere.core.persistence;
+
+public class UniqueID {
+
+    static long current = System.currentTimeMillis();
+
+    /**
+     * Gets a unqiue ID
+     * @return ID
+     */
+    public static synchronized String get() {
+       return Long.toString(current++);
+
+    }
+}
