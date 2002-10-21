04fff7a74932df8c9cc96fa892acd808c233c103
wehrens <wehrens@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2002-10-21 20:50:08 +0000

    modified this test first, most likley I delete it next time
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@337 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

--- tests/org/gridlab/gridsphere/services/user/UserManagerServiceTest.java
+++ tests/org/gridlab/gridsphere/services/user/UserManagerServiceTest.java
@@ -1,5 +1,6 @@
 /*
  * @author <a href="mailto:novotny@aei.mpg.de">Jason Novotny</a>
+ * @author <a href="mailto:oliver@wehrens.de">Oliver Wehrens</a>
  * @version $Id$
  */
 package org.gridlab.gridsphere.services.user;
@@ -19,16 +20,14 @@ import org.gridlab.gridsphere.services.user.UserManagerService;
 import org.gridlab.gridsphere.services.user.AccountRequest;
 import org.gridlab.gridsphere.services.user.PermissionDeniedException;
 import org.gridlab.gridsphere.services.user.impl.AccountRequestImpl;
+import org.gridlab.gridsphere.portletcontainer.descriptor.Role;
 
 import junit.framework.TestCase;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 import junit.framework.AssertionFailedError;
 
-import java.util.Properties;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Vector;
+import java.util.*;
 import java.io.FileNotFoundException;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -43,7 +42,7 @@ public class UserManagerServiceTest extends ServiceTest {
     public static final String[] GROUPS = {"cactus", "portals", "triana"};
     public static final String[] USERS = {"hans", "franz", "josef"};
 
-    private SportletUser hans, franz, josef;
+    private SportletUser hans, franz, josef, root, nobody;
     private PortletGroup cactus, portals, triana;
     private List groups;
 
@@ -74,7 +73,6 @@ public class UserManagerServiceTest extends ServiceTest {
         groups = new Vector(GROUPS.length);
         try {
             for (i = 0; i < GROUPS.length; i++) {
-                groups.set(i, GROUPS[i]);
                 aclManagerService.createNewGroup(GROUPS[i]);
             }
         } catch (PortletServiceException e) {
@@ -90,48 +88,46 @@ public class UserManagerServiceTest extends ServiceTest {
             fail("Unable to get all groups");
         }
 
-        cactus = (PortletGroup)portletGroups.get(0);
-        portals = (PortletGroup)portletGroups.get(1);
-        triana = (PortletGroup)portletGroups.get(2);
+        Iterator it = portletGroups.iterator();
 
-        // Make sure we have proper ordering
-        assertEquals(cactus.getName(), GROUPS[0]);
-        assertEquals(portals.getName(), GROUPS[1]);
-        assertEquals(triana.getName(), GROUPS[2]);
+        // get the groups, make sure we get it right, not rely on 1st, or 2nd dataset
+        while (it.hasNext()) {
+            PortletGroup sg = ((PortletGroup)it.next());
+            if (sg.getName().equals("cactus")) {
+                cactus = sg;
+            }
 
-        // Lets make user 0 the super user  and admin of group 0
-        hans = new SportletUserImpl();
-        hans.setGivenName(USERS[0]);
-        hans.setUserID(USERS[0]);
-        try {
-            aclManagerService.addUserToSuperRole(hans);
-            aclManagerService.addUserToGroup(hans, cactus, SportletRole.getAdminRole());
-        } catch (PortletServiceException e) {
-            log.error("Unable to add users: ", e);
-            fail("Unable to add users");
+            if (sg.getName().equals("portals")) {
+                portals = sg;
+            }
+
+            if (sg.getName().equals("triana")) {
+                triana = sg;
+            }
         }
 
-        // Lets make user 1 admin of group 1 and 2
-        franz = new SportletUserImpl();
-        franz.setGivenName(USERS[1]);
-        franz.setUserID(USERS[1]);
-        try {
-            aclManagerService.addUserToGroup(franz, portals, SportletRole.getAdminRole());
-            aclManagerService.addUserToGroup(franz, triana, SportletRole.getAdminRole());
-        } catch (PortletServiceException e) {
-            log.error("Unable to add users: ", e);
-            fail("Unable to add users");
-        }
+        // if one was not retrieved from the db it will fail
+        assertEquals(cactus.getName(), "cactus");
+        assertEquals(portals.getName(), "portals");
+        assertEquals(triana.getName(), "triana");
 
-        // Lets make user 2 admin of group 2
-        josef = new SportletUserImpl();
-        josef.setGivenName(USERS[2]);
-        josef.setUserID(USERS[2]);
+
+        log.info("----------------------------");
+
+
+        // we cannoy add users here since we don't have right now a root user
+
+        root = (SportletUserImpl)userManager.loginUser("root");
+        nobody = (SportletUserImpl)userManager.loginUser("nobody2");
+
+        // make nobody admin of portals group
+
+        log.info("RoleID: "+portals.getID());
+
         try {
-            aclManagerService.addUserToGroup(josef, triana, SportletRole.getAdminRole());
+            aclManagerService.addRoleInGroup(nobody, portals);
         } catch (PortletServiceException e) {
-            log.error("Unable to add users: ", e);
-            fail("Unable to add users");
+            log.error("Error :"+e);
         }
 
     }
@@ -141,30 +137,34 @@ public class UserManagerServiceTest extends ServiceTest {
     }
 
 
+
+
     public void testUserService() {
         AccountRequest req1, req2;
         // create mock accounts -- since createAccountRequest just returns new AccountRequest()
         // there's little need to test this method
 
+        log.info("Jason");
 
         // Create James -- who wants to be in portals and triana
-        List req1groups = new Vector();
-        req1groups.add(portals);
-        req1groups.add(triana);
-
         req1 = userManager.createAccountRequest();
-        req1.setFullName("James A Somebody");
-        req1.setUserID("somebody");
-        req1.setDesiredGroups(req1groups);
+        req1.setFullName("Jason");
+        req1.setUserID("jason");
 
-        // Create John -- who wants to be in cactus
-        List req2groups = new Vector();
-        req2groups.add(cactus);
+        req1.addToGroup(portals, SportletRole.getUserRole());
+        req1.addToGroup(triana, SportletRole.getUserRole());
+        req1.addToGroup(cactus,SportletRole.getUserRole());
 
+        log.info("Michael");
         req2 = userManager.createAccountRequest();
         req2.setFullName("John D Nobody");
         req2.setUserID("nobody");
-        req2.setDesiredGroups(req2groups);
+        req2.addToGroup(cactus, SportletRole.getUserRole());
+
+        log.info("Making user done");
+
+        log.info("-- request creating done ");
+
 
         // submit requests as well
         try {
@@ -175,6 +175,9 @@ public class UserManagerServiceTest extends ServiceTest {
             fail("Unable to submit account request");
         }
 
+
+        log.info("Requests sumbitted");
+
         List accounts = userManager.getAccountRequests();
         assertEquals(accounts.size(), 2);
 
@@ -184,58 +187,56 @@ public class UserManagerServiceTest extends ServiceTest {
         assertEquals(james.getFullName(), req1.getFullName());
         assertEquals(john.getFullName(), req2.getFullName());
 
+
+        log.info("two requests and names are ok");
+
         // Now lets see who can and can't approve deny requests
         // james wants to be in groups 1 and 2, the admins are franz and josef
         // hans is super user and can approve account but cannot approve group
 
-        // make sure franz cannot give james an account
+        // make sure nobody cannot give james an account
         try {
-            userManager.approveAccountRequest(franz, james, null);
+            userManager.approveAccountRequest(nobody, james, null);
             fail("Should raise a PermissionDeniedException");
         } catch (PermissionDeniedException e) {}
 
-        // Now hans CAN approve a new account
+        // Now root CAN approve a new account
         try {
-            userManager.approveAccountRequest(hans, james, null);
+            userManager.approveAccountRequest(root, james, null);
         } catch (PermissionDeniedException e) {
             fail("Should NOT raise a PermissionDeniedException");
         }
 
-        // Lets make sure josef cannot approve/deny a group request for triana
+        // root denies account for john
+
         try {
-            userManager.approveGroupRequest(josef, james, triana, null);
+            userManager.denyAccountRequest(root, john, null);
+        } catch (PermissionDeniedException e) {
+            fail("Should NOT raise a PermissionDeniedException");
+        }
+
+        // play around with groups
+        log.info("start playing around with group approvals");
+
+        SportletUserImpl userjames = (SportletUserImpl)userManager.loginUser(james.getUserID());
+
+        log.info("User James:"+userjames.getFullName());
+
+        // Lets make sure 'nobody' cannot approve/deny a group request for triana
+        try {
+            userManager.approveGroupRequest(nobody, userjames, triana, null);
             fail("Should raise a PermissionDeniedException");
-        } catch (PermissionDeniedException e) {}
+        } catch (PermissionDeniedException e) {
 
+        }
 
-        // Now let franz approve portals group
+        // Now let 'nobody' approve james to portals group, because he is portal admin
         try {
-            userManager.approveGroupRequest(franz, james, portals, null);
+            userManager.approveGroupRequest(nobody, userjames, portals, null);
         } catch (PermissionDeniedException e) {
             fail("Should NOT raise a PermissionDeniedException");
         }
 
-        // Now let josef deny triana group
-        try {
-            userManager.approveGroupRequest(josef, james, triana, null);
-        } catch (PermissionDeniedException e) {
-            fail("Should NOT raise a PermissionDeniedException");
-        }
-
-        // Now john is out of luck-- lets say hans is feeling schizophrenic
-        // hans approves the group request, yet denies the account
-
-        try {
-            userManager.approveGroupRequest(hans, john, cactus, null);
-        } catch (PermissionDeniedException e) {
-            fail("Should NOT raise a PermissionDeniedException");
-        }
-
-        try {
-            userManager.denyAccountRequest(hans, john, null);
-        } catch (PermissionDeniedException e) {
-            fail("Should NOT raise a PermissionDeniedException");
-        }
 
         // So now we should only have one user, james
         assertTrue(userManager.existsUser(james.getUserID()));
@@ -243,19 +244,19 @@ public class UserManagerServiceTest extends ServiceTest {
 
         List allUsers = userManager.getAllUsers();
         assertNotNull(allUsers);
-        assertEquals(allUsers.size(), 1);
+        assertEquals(allUsers.size(), 3);
 
-        User jamesUser = (User)allUsers.get(0);
-        assertEquals(jamesUser.getUserID(), james.getUserID());
+//        User jamesUser = (User)allUsers.get(0);
+//        assertEquals(jamesUser.getUserID(), james.getUserID());
 
         // See if james can edit his account -- needs super approval
         String newname = "James Cool Guy";
-        AccountRequest newreq = userManager.changeAccountRequest(jamesUser);
+        AccountRequest newreq = userManager.changeAccountRequest(userjames);
         newreq.setFullName(newname);
 
         try {
             userManager.submitAccountRequest(newreq);
-            userManager.approveAccountRequest(hans, newreq, null);
+            userManager.approveAccountRequest(root, newreq, null);
         } catch (PermissionDeniedException e) {
             fail("Should NOT raise a PermissionDeniedException");
         } catch (PortletServiceException e) {
@@ -264,11 +265,11 @@ public class UserManagerServiceTest extends ServiceTest {
         }
 
         // test login
-        User user = userManager.loginUser(jamesUser.getUserID());
+        User user = userManager.loginUser(userjames.getUserID());
         List activeUsers = userManager.getActiveUsers();
         assertEquals(activeUsers.size(), 1);
         User auser = (User)activeUsers.get(0);
-        assertEquals(auser.getUserID(), jamesUser.getUserID());
+        assertEquals(auser.getUserID(), userjames.getUserID());
 
         // test logoff
         userManager.logoffUser(auser);
@@ -277,12 +278,12 @@ public class UserManagerServiceTest extends ServiceTest {
 
         // test remove user
         try {
-            userManager.removeUser(franz, jamesUser.getUserID());
+            userManager.removeUser(franz, userjames.getUserID());
             fail("Should raise a PermissionDeniedException");
         } catch (PermissionDeniedException e) {}
 
         try {
-            userManager.removeUser(hans, jamesUser.getUserID());
+            userManager.removeUser(hans, userjames.getUserID());
         } catch (PermissionDeniedException e) {
             fail("Should NOT raise a PermissionDeniedException");
         }
