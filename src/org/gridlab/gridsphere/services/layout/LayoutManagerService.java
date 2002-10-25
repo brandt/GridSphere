b66afc2a8ec8df98690187c648a375fcdc04de20
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2002-10-25 11:49:51 +0000

    *** empty log message ***
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@365 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

--- src/org/gridlab/gridsphere/services/layout/LayoutManagerService.java
+++ src/org/gridlab/gridsphere/services/layout/LayoutManagerService.java
@@ -2,16 +2,32 @@
  * @author <a href="mailto:novotny@aei.mpg.de">Jason Novotny</a>
  * @version $Id$
  */
-package org.gridlab.gridsphere.layout;
+package org.gridlab.gridsphere.services.layout;
 
 import org.gridlab.gridsphere.portlet.service.PortletService;
+import org.gridlab.gridsphere.portlet.User;
+import org.gridlab.gridsphere.layout.PortletLayoutException;
+
+import java.util.List;
 
 
 /**
- * The LayoutManagerService
+ * The LayoutManagerService provies customization support for user layouts
  */
 public interface LayoutManagerService extends PortletService {
 
+    public void addPortletFrame(User user, String concretePortletID) throws PortletLayoutException;
 
+    public void removePortletFrame(User user, String concretePortletID);
+
+    public List getPortletFrames(User user);
+
+    public List getPortletFrames(User user, String tab);
+
+    public void addPortletTab(User user, String tabName);
+
+    public void removePortletTab(User user, String tabName);
+
+    public List getPortletTabs(User user);
 
 }
