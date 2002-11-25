304efadc9e14a36121810fa021f15ce31fa2690c
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2002-11-25 14:15:47 +0000

    changed PortletApplication to PortletApp
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@457 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/portletcontainer/descriptor/PortletApp.java
@@ -0,0 +1,111 @@
+/*
+ * @author <a href="mailto:oliver@wehrens.de">Oliver Wehrens</a>
+ * @author <a href="mailto:novotny@aei.mpg.de">Jason Novotny</a>
+ * @version $Id$
+ */
+
+package org.gridlab.gridsphere.portletcontainer.descriptor;
+
+import java.util.Vector;
+import java.util.List;
+
+public class PortletApp {
+
+    private String id = new String();
+    private String portletName = new String();
+    private String servletName = new String();
+    private List portletConfig = new Vector();
+    private CacheInfo cacheInfo = new CacheInfo();
+    private AllowsWindowStates allowsWindowStates = new AllowsWindowStates();
+    private SupportsModes supportsModes = new SupportsModes();
+
+
+    public PortletApp() {}
+
+    /**
+     * Returns the id of a PortletApplication
+     *
+     * @returns id of the PortletApplication
+     */
+    public String getID() {
+        return id;
+    }
+
+    /**
+     * sets the id of a PortletApplication
+     *
+     * @param id the uid of the PortletApplication
+     */
+    public void setID(String id) {
+        this.id = id;
+    }
+
+    /**
+     * Returns the name of a servlet associated with this portlet defined in web.xml as <servlet-name>
+     *
+     * @returns the servlet name
+     */
+    public String getServletName() {
+        return servletName;
+    }
+
+    /**
+     * sets the servlet name associated with this portlet defined in web.xml as <servlet-name>
+     *
+     * @param name the servlet name
+     */
+    public void setServletName(String servletName) {
+        this.servletName = servletName;
+    }
+
+    /**
+     * gets the name of a PortletApplication
+     *
+     * @returns name of the PortletApplication
+     */
+    public String getPortletName() {
+        return portletName;
+    }
+
+    /**
+     * sets the name of a PortletApplication
+     *
+     * @param name name of a PortletApplication
+     */
+    public void setPortletName(String portletName) {
+        this.portletName = portletName;
+    }
+
+    public List getConfigParamList() {
+        return portletConfig;
+    }
+
+    public void setConfigParamList(Vector portletConfig) {
+        this.portletConfig = portletConfig;
+    }
+
+    public CacheInfo getCacheInfo() {
+        return cacheInfo;
+    }
+
+    public void setCacheInfo(CacheInfo cacheInfo) {
+        this.cacheInfo = cacheInfo;
+    }
+
+    public void setAllowsWindowStates(AllowsWindowStates allowsWindowStates) {
+        this.allowsWindowStates = allowsWindowStates;
+    }
+
+    public AllowsWindowStates getAllowsWindowStates() {
+        return allowsWindowStates;
+    }
+
+    public void setSupportsModes(SupportsModes supportModes) {
+        this.supportsModes = supportModes;
+    }
+
+    public SupportsModes getSupportsModes() {
+        return supportsModes;
+    }
+}
+
