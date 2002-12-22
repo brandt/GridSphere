f223e0cbed96831e0659c4b8d40bcf34c5808b12
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2002-12-22 11:19:31 +0000

    BasePortletLifecycle just has empty methods
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@592 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/layout/BasePortletLifecycle.java
@@ -0,0 +1,38 @@
+/**
+ * @author <a href="mailto:novotny@aei.mpg.de">Jason Novotny</a>
+ * @version $Id$
+ */
+package org.gridlab.gridsphere.layout;
+
+import org.gridlab.gridsphere.portletcontainer.GridSphereEvent;
+
+import java.io.IOException;
+import java.util.List;
+
+public abstract class BasePortletLifecycle implements PortletLifecycle {
+
+    protected int COMPONENT_ID = -1;
+
+    public  List init(List list) {
+        COMPONENT_ID = list.size();
+        return list;
+    }
+
+    public void login(GridSphereEvent event) {}
+
+    public void logout(GridSphereEvent event) {}
+
+    public void destroy() {}
+
+    public int getComponentID() {
+        return COMPONENT_ID;
+    }
+
+    public void actionPerformed(GridSphereEvent event) throws PortletLayoutException, IOException {
+
+    }
+
+    public void doRender(GridSphereEvent event) throws PortletLayoutException, IOException {
+
+    }
+}
