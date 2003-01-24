f34ef29fe5614a95751f0fa1a91e92a5a6596b45
wehrens <wehrens@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2003-01-24 14:35:32 +0000

    new submit button tag
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@774 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/tags/web/SubmitButtonTag.java
@@ -0,0 +1,17 @@
+/*
+ * @author <a href="wehrens@aei.mpg.de">Oliver Wehrens</a>
+ * @version $Id$
+ */
+
+package org.gridlab.gridsphere.tags.web;
+
+import javax.servlet.jsp.JspException;
+
+public class SubmitButtonTag extends InputTag {
+
+    public int doStartTag() throws JspException {
+        type = SUBMIT;
+        name = "submit:"+name;
+        return super.doStartTag();
+    }
+}
