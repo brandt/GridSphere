d63957a0f51ab62468c7afec62a554659f9685ea
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2003-01-26 18:33:31 +0000

    fixed portlet manager portlet start and stop methods
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@784 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

--- src/org/gridlab/gridsphere/portlets/manager/PortletApplicationManager.java
+++ src/org/gridlab/gridsphere/portlets/manager/PortletApplicationManager.java
@@ -8,10 +8,9 @@ import org.gridlab.gridsphere.event.ActionEvent;
 import org.gridlab.gridsphere.portlet.*;
 import org.gridlab.gridsphere.portlet.service.PortletServiceNotFoundException;
 import org.gridlab.gridsphere.portlet.service.PortletServiceUnavailableException;
-import org.gridlab.gridsphere.services.registry.PortletRegistryService;
+import org.gridlab.gridsphere.services.registry.PortletManagerService;
 import org.gridlab.gridsphere.portlets.manager.tomcat.TomcatManagerWrapper;
 import org.gridlab.gridsphere.portlets.manager.tomcat.TomcatWebAppResult;
-import org.gridlab.gridsphere.portletcontainer.UserPortletManager;
 
 import javax.servlet.UnavailableException;
 import java.io.IOException;
@@ -25,14 +24,13 @@ import java.util.Map;
  */
 public class PortletApplicationManager extends AbstractPortlet {
 
-    private TomcatManagerWrapper manager = TomcatManagerWrapper.getInstance();
-
-    private PortletRegistryService registryService = null;
+    private PortletManagerService portletManager = null;
+    private TomcatManagerWrapper tomcat = TomcatManagerWrapper.getInstance();
 
     public void init(PortletConfig config) throws UnavailableException {
         super.init(config);
         try {
-            registryService = (PortletRegistryService)config.getContext().getService(PortletRegistryService.class);
+            portletManager = (PortletManagerService)config.getContext().getService(PortletManagerService.class);
         } catch (PortletServiceUnavailableException e) {
             System.err.println("PortletRegistry service unavailable! ");
         } catch (PortletServiceNotFoundException e) {
@@ -42,6 +40,8 @@ public class PortletApplicationManager extends AbstractPortlet {
 
     public void actionPerformed(ActionEvent event) throws PortletException {
         DefaultPortletAction action = event.getAction();
+        PortletRequest req = event.getPortletRequest();
+        PortletResponse res = event.getPortletResponse();
         User user = event.getPortletRequest().getUser();
         Map params = action.getParameters();
         String operation = (String)params.get("operation");
@@ -49,28 +49,27 @@ public class PortletApplicationManager extends AbstractPortlet {
         TomcatWebAppResult result = null;
         if ((operation != null) && (appName!= null)) {
             if (operation.equals("start")) {
-                result = manager.startWebApp(appName);
+                result = tomcat.startWebApp(appName);
+                portletManager.installPortletWebApplication(appName, req, res);
             } else if (operation.equals("stop")) {
-                result = manager.stopWebApp(appName);
-                registryService.removePortletWebApplication(user, appName);
+                portletManager.destroyPortletWebApplication(appName, req, res);
+                result = tomcat.stopWebApp(appName);
             } else if (operation.equals("reload")) {
-                result = manager.reloadWebApp(appName);
-                registryService.reloadPortletWebApplication(user, appName);
-                UserPortletManager userPortletManager = UserPortletManager.getInstance();
-                userPortletManager.init(event.getPortletRequest(), event.getPortletResponse());
+                result = tomcat.reloadWebApp(appName);
+                portletManager.initPortletWebApplication(appName, req, res);
             } else if (operation.equals("remove")) {
-                result = manager.removeWebApp(appName);
-                registryService.removePortletWebApplication(user, appName);
+                portletManager.removePortletWebApplication(appName, req, res);
+                result = tomcat.removeWebApp(appName);
             } else if (operation.equals("deploy")) {
-                result = manager.deployWebApp(appName);
-                registryService.addPortletWebApplication(user, appName);
+                result = tomcat.deployWebApp(appName);
+                portletManager.installPortletWebApplication(appName, req, res);
             } else if (operation.equals("undeploy")) {
-                result = manager.undeployWebApp(appName);
-                registryService.removePortletWebApplication(user, appName);
-            } else if (operation.equals("deploy")) {
+                result = tomcat.undeployWebApp(appName);
+                portletManager.removePortletWebApplication(appName, req, res);
+            } else if (operation.equals("install")) {
                 String warFile = (String)params.get("warfile");
-                result = manager.installWebApp(appName, warFile);
-                registryService.addPortletWebApplication(user, appName);
+                result = tomcat.installWebApp(appName, warFile);
+                portletManager.installPortletWebApplication(appName, req, res);
             }
         }
         if (result != null) System.err.println("result: " + result.getReturnCode() + " " + result.getDescription());
@@ -78,19 +77,12 @@ public class PortletApplicationManager extends AbstractPortlet {
 
 
     public void doView(PortletRequest request, PortletResponse response) throws PortletException, IOException {
-        List webapps = registryService.listPortletWebApplications();
+        List webapps = portletManager.listPortletWebApplications();
         for (int i = 0; i < webapps.size(); i++) {
             System.err.println("webapp " + i + " " + webapps.get(i));
         }
-        TomcatWebAppResult result = manager.getWebAppList();
+        TomcatWebAppResult result = tomcat.getWebAppList();
         List allwebapps = result.getWebAppDescriptions();
-        //Iterator it = webapps.iterator();
-        //List newlist = new Vector();
-        //while (it.hasNext()) {
-        //    TomcatWebAppResult.TomcatWebAppDescription d = (TomcatWebAppResult.TomcatWebAppDescription)it.next();
-        //    if (list.contains(d.getContextPath().substring(1)))
-        // }
-
         System.err.println(result.getReturnCode() + " : " + result.getDescription());
         request.setAttribute("result", result);
         getPortletConfig().getContext().include("/jsp/list.jsp", request, response);
