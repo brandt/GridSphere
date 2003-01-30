d4717d143a3ecce28ca7a8e1d627c05c389ba683
wehrens <wehrens@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2003-01-30 21:50:27 +0000

    new FormEvent impl
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@825 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

--- src/org/gridlab/gridsphere/event/impl/FormEventImpl.java
+++ src/org/gridlab/gridsphere/event/impl/FormEventImpl.java
@@ -9,7 +9,11 @@ import org.gridlab.gridsphere.event.FormEvent;
 import org.gridlab.gridsphere.event.ActionEvent;
 import org.gridlab.gridsphere.portlet.PortletRequest;
 
-import java.util.Enumeration;
+import org.gridlab.gridsphere.tags.web.model.CheckBoxItem;
+import org.gridlab.gridsphere.tags.web.model.CheckBoxModel;
+import org.gridlab.gridsphere.tags.web.model.ListBoxModel;
+
+import java.util.*;
 
 public class FormEventImpl implements FormEvent {
 
@@ -33,13 +37,76 @@ public class FormEventImpl implements FormEvent {
         while(enum.hasMoreElements()) {
             String name = (String)enum.nextElement();
             if (name.startsWith("submit:")) {
-                Object ob = req.getParameter((String)enum.nextElement());
-                if (ob!=null) {
+                String button = req.getParameter(name);
+                if (button!=null) {
                     result = name.substring(7);
                 }
             }
         }
         return result;
+    }
+
+    public String getRadioButton() {
+        return null;
+    }
+
+    private String[] getSelectedItem(String groupname) {
+
+        PortletRequest req = event.getPortletRequest();
+        Enumeration enum = req.getParameterNames();
+        while(enum.hasMoreElements()) {
+            String name = (String)enum.nextElement();
+            if (name.equals(groupname)) {
+                return req.getParameterValues(name);
+            }
+        }
+        return null;
+
+    }
+
+    /**
+     * Gets the list of selected values of a listbox , return null if it does not exist
+     * @param listboxname name of the listbox
+     * @return array of stringvalues with the values of the listboxes
+     */
+    public String[] getSelectedListBoxValues (String listboxname) {
+        return getSelectedItem(listboxname);
+    }
+
+    /**
+     * Gets the list of values of selected checkboxes in a group (means with the same name), returns null if does not exist
+     * @param checkboxgroupname name of the checkboxgroup
+     * @return array of stringvalues with the values of the checkboxes
+     */
+    public String[] getSelectedCheckBoxValues (String checkboxgroupname) {
+        PortletRequest req = event.getPortletRequest();
+        return req.getParameterValues(checkboxgroupname);
+    }
+
+    /**
+     * Modifies the passed in checkboxmodel to reflect the changes on the checkboxes
+     * @model the model
+     */
+    public CheckBoxModel adjustCheckBoxModel(CheckBoxModel model) {
+
+        model.unselectAll();
+
+        /*CheckBoxModel model = new CheckBoxModel();
+
+
+        String result[] = getSelectedCheckBoxValues(modelname);
+
+        model.unselectAll();
+        for (int i=0;i<result.length;i++) {
+            ((CheckBoxItem)model.getItem(result[i])).setSelected(true);
+        } */
+
+        return model;
+    }
+
+    public ListBoxModel adjustListBoxModel(ListBoxModel model) {
+        //model.unselectAll();
+        return model;
     }
 
 }
