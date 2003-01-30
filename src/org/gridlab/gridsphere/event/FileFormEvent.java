87b7318e87b5624c9375a2f569c19bbd3aa599cc
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2003-01-30 12:46:47 +0000

    form for handling file uploads using Jakarta commons library
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@813 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/event/FileFormEvent.java
@@ -0,0 +1,17 @@
+/*
+ * @author <a href="wehrens@aei.mpg.de">Oliver Wehrens</a>
+ * @versiob $Id$
+ */
+
+package org.gridlab.gridsphere.event;
+
+import java.io.File;
+
+public interface FileFormEvent extends FormEvent {
+
+    public static final int MAX_UPLOAD_SIZE = 1024 * 1024;
+    public static final String TEMP_DIR = "/tmp";
+
+    public File saveFile(String filePath) throws FileFormException;
+
+}
---
+++ src/org/gridlab/gridsphere/event/impl/FileFormEventImpl.java
@@ -0,0 +1,69 @@
+/*
+ * @author <a href="wehrens@aei.mpg.de">Oliver Wehrens</a>
+ * @version $Id$
+ */
+
+package org.gridlab.gridsphere.event.impl ;
+
+import org.gridlab.gridsphere.event.FormEvent;
+import org.gridlab.gridsphere.event.ActionEvent;
+import org.gridlab.gridsphere.event.FileFormEvent;
+import org.gridlab.gridsphere.event.FileFormException;
+import org.gridlab.gridsphere.portlet.PortletRequest;
+import org.gridlab.gridsphere.portlet.PortletException;
+import org.apache.commons.fileupload.FileUpload;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
+import java.io.File;
+
+public class FileFormEventImpl extends FormEventImpl implements FileFormEvent {
+
+    private boolean haveFile = false;
+
+    public FileFormEventImpl(ActionEvent evt) {
+        super(evt);
+    }
+
+    public File saveFile(String filePath) throws FileFormException {
+
+        if (!filePath.endsWith("/")) filePath += "/";
+
+         // Create a new file upload handler
+        FileUpload upload = new FileUpload();
+
+        // Set upload parameters
+        upload.setSizeMax(MAX_UPLOAD_SIZE);
+        upload.setRepositoryPath(TEMP_DIR);
+
+        // Parse the request
+        List items = null;
+        try {
+            items = upload.parseRequest(event.getPortletRequest());
+        } catch (FileUploadException e) {
+            throw new FileFormException("Unable to parse uploaded file: ", e);
+        }
+        // Process the uploaded fields
+        Iterator iter = items.iterator();
+        File file = null;
+        try {
+            while (iter.hasNext()) {
+                FileItem item = (FileItem) iter.next();
+                if (item.isFormField()) {
+
+                    item.write(filePath + item.getName());
+                    file = item.getStoreLocation();
+                    System.err.println("file is : " + file.getAbsolutePath());
+                }
+            }
+            haveFile = true;
+        } catch (Exception e) {
+            throw new FileFormException("Unable to save file", e);
+        }
+        return file;
+    }
+
+}
