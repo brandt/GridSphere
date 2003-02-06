170111922925281feb7f9e1bb056c3440e1e0f9b
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2003-02-06 12:31:08 +0000

    *** empty log message ***
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@859 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/portlet/service/PortletServiceAuthorizationException.java
@@ -0,0 +1,17 @@
+/*
+ * @author <a href="mailto:novotny@aei.mpg.de">Jason Novotny</a>
+ * @version $Id$
+ */
+package org.gridlab.gridsphere.portlet.service;
+
+public class PortletServiceAuthorizationException extends RuntimeException {
+
+    public PortletServiceAuthorizationException() {
+        super();
+    }
+
+    public PortletServiceAuthorizationException(String msg) {
+        super(msg);
+    }
+}
+
