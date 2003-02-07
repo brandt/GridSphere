c4c677087e487f378cb33b469c559440f5efc898
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2003-02-07 18:36:22 +0000

    *** empty log message ***
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@880 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

--- tests/org/gridlab/gridsphere/portlet/service/spi/ServiceTest.java
+++ tests/org/gridlab/gridsphere/portlet/service/spi/ServiceTest.java
@@ -28,17 +28,13 @@ public class ServiceTest extends TestCase {
 
     protected void setUp() {
         PropertyConfigurator.configure("conf/log4j.properties");
-        factory = SportletServiceFactory.getInstance();
-        if (factory == null) fail("Unable to instantiate SportletServiceFactory!");
     }
 
-    /*
     public void testCreateFactory() {
         // create factory
         factory = SportletServiceFactory.getInstance();
         if (factory == null) fail("Unable to instantiate SportletServiceFactory!");
     }
-     */
 
     public static void main(String[] args) throws Exception {
         junit.textui.TestRunner.run(suite());
