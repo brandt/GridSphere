680da6334cdae82afb26f0c694c8185fb1402009
russell <russell@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2003-02-19 17:26:54 +0000

    *** empty log message ***
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@959 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/services/core/security/password/impl/DbmsPassword.java
@@ -0,0 +1,35 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: russell
+ * Date: Jan 8, 2003
+ * Time: 11:39:09 AM
+ * To change template for new class use 
+ * Code Style | Class Templates options (Tools | IDE Options).
+ */
+package org.gridlab.gridsphere.services.core.security.password.impl;
+
+import org.gridlab.gridsphere.services.core.security.password.Password;
+import org.gridlab.gridsphere.portlet.User;
+import org.gridlab.gridsphere.portlet.impl.SportletUserImpl;
+import org.gridlab.gridsphere.core.persistence.BaseObject;
+
+import java.util.Date;
+
+public interface DbmsPassword extends Password  {
+
+    public void setUser(User user);
+
+    public void setHint(String hint);
+
+    public String getValue();
+
+    public void setValue(String value);
+
+    public void setLifetime(long lifetime);
+
+    public void setDateExpires(Date dateExpires);
+
+    public void setDateCreated(Date dateCreated);
+
+    public void setDateLastModified(Date dateModified);
+}
