e2d786a9dabe005dc9a12d7496bbd1ac56ac9272
wehrens <wehrens@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2003-02-19 14:46:45 +0000

    events for tags
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@936 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/tags/event/FormEvent.java
@@ -0,0 +1,53 @@
+/*
+ * @author <a href="wehrens@aei.mpg.de">Oliver Wehrens</a>
+ * @versiob $Id$
+ */
+
+package org.gridlab.gridsphere.tags.event;
+
+
+import org.gridlab.gridsphere.tags.web.element.ElementBean;
+
+import java.util.List;
+import java.util.Collection;
+
+public interface FormEvent {
+
+   /**
+    * Returns the name of the pressed submit button.
+    * @parameter event the actionevent
+    * @return name of the button which was pressed
+    */
+    public String getPressedSubmitButton();
+
+    public String getRadioButton();
+
+    /**
+     * Gets the list of selected values of a listbox
+     * @param listboxname name of the listbox
+     * @return array of stringvalues with the values of the listboxes
+     */
+    public String[] getSelectedListBoxValues (String listboxname);
+
+    /**
+     * Gets the list of values of selected checkboxes in a group (means with the same name), returns null if does not exist
+     * @param checkboxgroupname name of the checkboxgroup
+     * @return array of stringvalues with the values of the checkboxes
+     */
+    public String[] getSelectedCheckBoxValues (String checkboxgroupname);
+
+    /**
+     * Gets back the original saved bean.
+     * @param name name of the bean
+     * @return ElementBean
+     */
+    public Object getElementBean(String name);
+
+    /**
+     * Gets back the bean with the modifications from the userinterface.
+     * @param name name of the bean
+     * @return updated elementbean
+     */
+    public Object getUpdatedElementBean(String name);
+
+}
