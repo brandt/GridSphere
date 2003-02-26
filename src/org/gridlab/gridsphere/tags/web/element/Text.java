1b61531815838859223b4557c91e24b6e93d0a0e
wehrens <wehrens@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2003-02-26 10:02:24 +0000

    refactor
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@1020 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/tags/web/element/Text.java
@@ -0,0 +1,23 @@
+/**
+ * @author <a href="oliver.wehrens@aei.mpg.de">Oliver Wehrens</a>
+ * @version $Id$
+ */
+
+package org.gridlab.gridsphere.tags.web.element;
+
+public interface Text extends Element {
+
+    /**
+     * Gets the label of the bean.
+     * @return label of the bean
+     */
+    public String getLabel();
+
+    /**
+     * Sets the label of the bean
+     * @param label the label to be set
+     */
+    public void setLabel(String label);
+
+
+}
