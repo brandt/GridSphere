6f45850f958ee6767923bb0809a9ab6a9281e24f
wehrens <wehrens@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2003-02-26 10:38:18 +0000

    new label
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@1022 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/tags/web/element/LabelBean.java
@@ -0,0 +1,30 @@
+/*
+ * @author <a href="oliver.wehrens@aei.mpg.de">Oliver Wehrens</a>
+ * @version $Id$
+ */
+
+package org.gridlab.gridsphere.tags.web.element;
+
+public class LabelBean extends NameBean implements Valueable {
+
+    protected String value = new String();
+
+    /**
+     * Sets the value of the bean.
+     * @param value the value
+     */
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    /**
+     * Gets the value of the bean.
+     * @return the value
+     */
+    public String getValue() {
+        return value;
+    }
+
+    public void update(String[] values) {}
+
+}
