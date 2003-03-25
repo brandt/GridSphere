d834b2d713842d584d598adec8f1380eeb97fd09
russell <russell@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2003-03-25 10:31:01 +0000

    *. Adding job manager portlets... forgot to add earlier!
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@1261 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/portlets/grid/job/JobManagerBean.java
@@ -0,0 +1,387 @@
+/*
+ * @author <a href="mailto:michael.russell@aei.mpg.de">Michael Russell</a>
+ * @version $Id$
+ */
+package org.gridlab.gridsphere.portlets.grid.job;
+
+import org.gridlab.gridsphere.portlets.PortletBean;
+import org.gridlab.gridsphere.services.grid.job.*;
+import org.gridlab.gridsphere.services.grid.job.impl.grms.*;
+import org.gridlab.gridsphere.services.grid.security.credential.Credential;
+import org.gridlab.gridsphere.portlet.*;
+
+import java.net.MalformedURLException;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+public class JobManagerBean extends PortletBean {
+
+    // User job pages
+    public static final String PAGE_USER_JOB_LIST = "/jsp/job/userJobList.jsp";
+    public static final String PAGE_USER_JOB_VIEW = "/jsp/job/userJobView.jsp";
+    public static final String PAGE_USER_JOB_EDIT = "/jsp/job/userJobEditApplication.jsp";
+    public static final String PAGE_USER_JOB_APPLICATION_EDIT = "/jsp/job/userJobEditApplication.jsp";
+    public static final String PAGE_USER_JOB_RESOURCES_EDIT = "/jsp/job/userJobEditResources.jsp";
+    public static final String PAGE_USER_JOB_EDIT_VERIFY = "/jsp/job/userJobEditVerify.jsp";
+    public static final String PAGE_USER_JOB_MIGRATE = "/jsp/job/userJobMigrate.jsp";
+    public static final String PAGE_USER_JOB_SPEC_EDIT = "/jsp/job/userJobEditSpecification.jsp";
+    public static final String PAGE_USER_JOB_SPEC_EDIT_VERIFY = "/jsp/job/userJobSpecEditVerify.jsp";
+    public static final String PAGE_USER_JOB_SPEC_EDIT_SUBMIT = "/jsp/job/userJobSpecEditSubmit.jsp";
+    public static final String PAGE_USER_JOB_SPEC_EDIT_CANCEL = "/jsp/job/userJobSpecEditCancel.jsp";
+
+    // Job manager variables
+    private JobManagerService jobManagerService = null;
+    private String jobManagerDescription = null;
+
+    // User job variables
+    private List userJobList = null;
+    private Job userJob = null;
+
+
+    /******************************************
+     * Portlet bean methods
+     ******************************************/
+
+    public JobManagerBean() {
+        initView();
+    }
+
+    public JobManagerBean(PortletConfig config, PortletRequest request, PortletResponse response)
+            throws PortletException {
+        init(config, request, response);
+        initServices();
+        initView();
+    }
+
+    protected void initView() {
+        setTitle("Job Manager");
+    }
+
+    protected void initServices()
+            throws PortletException {
+        this.log.debug("Entering initServices()");
+        this.jobManagerService = (JobManagerService) getPortletService(JobManagerService.class);
+        this.log.debug("Exiting initServices()");
+    }
+
+    public void doDefaultViewAction()
+            throws PortletException {
+        doListUserJob();
+    }
+
+    /******************************************
+     * Job manager methods
+     ******************************************/
+
+    public void loadJobManagerDescription()
+            throws JobManagerException {
+        User user = getPortletUser();
+        this.jobManagerDescription = jobManagerService.getJobManagerDescription(user);
+    }
+
+    public String getJobManagerDescription()
+            throws JobManagerException {
+        return this.jobManagerDescription;
+    }
+
+    /******************************************
+     * User job methods
+     ******************************************/
+
+    public void doListUserJob()
+            throws PortletException {
+        loadUserJobList();
+        setPage(PAGE_USER_JOB_LIST);
+    }
+
+    public void doViewUserJob()
+            throws PortletException {
+        loadUserJob();
+        setPage(PAGE_USER_JOB_VIEW);
+    }
+
+    public void doNewUserJob()
+            throws PortletException {
+        setPage(PAGE_USER_JOB_APPLICATION_EDIT);
+    }
+
+    public void doStageUserJob()
+            throws PortletException {
+        loadUserJob();
+        setPage(PAGE_USER_JOB_APPLICATION_EDIT);
+    }
+
+    public void doEditUserJobApplication()
+            throws PortletException {
+        loadUserJob();
+        editUserJob();
+        validateUserJobApplication();
+        setPage(PAGE_USER_JOB_APPLICATION_EDIT);
+    }
+
+    public void doEditUserJobResources()
+            throws PortletException {
+        loadUserJob();
+        editUserJob();
+        try {
+            validateUserJobApplication();
+            setPage(PAGE_USER_JOB_RESOURCES_EDIT);
+        } catch (Exception e) {
+            setPage(PAGE_USER_JOB_APPLICATION_EDIT);
+        }
+    }
+
+    public void doVerifyEditUserJob()
+            throws PortletException {
+        loadUserJob();
+        editUserJob();
+        try {
+            validateUserJobResources();
+            setPage(PAGE_USER_JOB_EDIT_VERIFY);
+        } catch (Exception e) {
+            setPage(PAGE_USER_JOB_RESOURCES_EDIT);
+        }
+    }
+
+    public void doSubmitEditUserJob()
+            throws PortletException {
+        loadUserJob();
+        editUserJob();
+        submitUserJob();
+        setPage(PAGE_USER_JOB_RESOURCES_EDIT);
+    }
+
+    public void doCancelEditUserJob()
+            throws PortletException {
+        loadUserJobList();
+        setPage(PAGE_USER_JOB_LIST);
+    }
+
+    public void doMigrateUserJob()
+            throws PortletException {
+        loadUserJob();
+        setPage(PAGE_USER_JOB_MIGRATE);
+    }
+
+    public void doSubmitMigrateUserJob()
+            throws PortletException {
+        loadUserJob();
+        migrateUserJob();
+        setPage(PAGE_USER_JOB_MIGRATE);
+    }
+
+    public void loadUserJobList() {
+        try {
+            User user = getPortletUser();
+            this.userJobList = jobManagerService.getJobList(user);
+        } catch (JobManagerException e) {
+            this.log.error("Unable to load portlet user job list", e);
+            this.userJobList = new Vector();
+        }
+    }
+
+    public void validateUserJobApplication()
+            throws PortletException {
+    }
+
+    public void validateUserJobResources()
+            throws PortletException {
+    }
+
+    public void loadUserJob() {
+        String jobID = getParameter("jobID");
+        if (!jobID.equals("")) {
+            User user = getPortletUser();
+            try {
+                this.userJob = jobManagerService.getJob(user, jobID);
+            } catch (Exception e) {
+                this.log.error("Unable to load portlet user job list", e);
+                this.userJobList = new Vector();
+            }
+        }
+    }
+
+    public void editUserJob() {
+    }
+
+    public GrmsJob submitUserJob()
+            throws PortletException {
+        User user = getPortletUser();
+        try {
+            GrmsJobSpecification jobSpecification = getJobSpecification();
+            return (GrmsJob) jobManagerService.submitJob(user, jobSpecification);
+        } catch (Exception e) {
+            this.log.error("Unable to submit user job", e);
+            throw new PortletException(e.getMessage());
+        }
+    }
+
+    public void migrateUserJob()
+            throws PortletException {
+        User user = getPortletUser();
+        try {
+            Job job = getUserJob();
+            jobManagerService.migrateJob(user, job);
+        } catch (Exception e) {
+            this.log.error("Unable to migrate user job", e);
+            throw new PortletException(e.getMessage());
+        }
+    }
+    public List getUserJobList() {
+        return this.userJobList;
+    }
+
+    public GrmsJob getUserJob()
+            throws JobNotFoundException, JobManagerException {
+        return (GrmsJob) userJob;
+    }
+
+    public GrmsJobSpecification getJobSpecification()
+            throws JobSpecificationException, MalformedURLException {
+        GrmsJobSpecification jobSpecification = null;
+        String jobSpecificationText = getParameter("jobSpecification");
+        if (jobSpecificationText == null) {
+            jobSpecification = new GrmsJobSpecification();
+            SingleJobDescription jobDescription = buildJobDescription();
+            jobSpecification.setJobDescription(jobDescription);
+        } else {
+            jobSpecification = GrmsJobSpecification.unmarshal(jobSpecificationText);
+        }
+        return jobSpecification;
+    }
+
+    public SingleJobDescription getJobDescription()
+            throws JobSpecificationException, MalformedURLException {
+        GrmsJobSpecification jobSpecification = null;
+        SingleJobDescription jobDescription = null;
+        String jobSpecificationText = getParameter("jobSpecification");
+        if (jobSpecificationText == null) {
+            jobDescription = buildJobDescription();
+        } else {
+            jobSpecification = GrmsJobSpecification.unmarshal(jobSpecificationText);
+            jobDescription = (SingleJobDescription) jobSpecification.getJobDescription();
+        }
+        return jobDescription;
+    }
+
+    public ApplicationDescription getApplicationDescription()
+            throws JobSpecificationException, MalformedURLException {
+        ApplicationDescription applicationDescription = null;
+        String jobSpecificationText = getParameter("jobSpecification");
+        if (jobSpecificationText == null) {
+            applicationDescription = buildApplicationDescription();
+        } else {
+            GrmsJobSpecification jobSpecification =
+                    GrmsJobSpecification.unmarshal(jobSpecificationText);
+            SingleJobDescription jobDescription =
+                    (SingleJobDescription) jobSpecification.getJobDescription();
+            applicationDescription = jobDescription.getApplicationDescription();
+        }
+        return applicationDescription;
+    }
+
+    public ResourceDescription getResourceDescription()
+            throws JobSpecificationException {
+        ResourceDescription resourceDescription = null;
+        String jobSpecificationText = getParameter("jobSpecification");
+        if (jobSpecificationText == null) {
+            resourceDescription = buildResourceDescription();
+        } else {
+            GrmsJobSpecification jobSpecification =
+                    GrmsJobSpecification.unmarshal(jobSpecificationText);
+            SingleJobDescription jobDescription =
+                    (SingleJobDescription) jobSpecification.getJobDescription();
+            resourceDescription = jobDescription.getResourceDescription();
+        }
+        return resourceDescription;
+    }
+
+    private SingleJobDescription buildJobDescription()
+            throws JobSpecificationException, MalformedURLException {
+        ResourceDescription resourceDescription =
+                buildResourceDescription();
+        ApplicationDescription applicationDescription =
+                buildApplicationDescription();
+        SingleJobDescription jobDescription = new SingleJobDescription();
+        jobDescription.setResourceDescription(resourceDescription);
+        jobDescription.setApplicationDescription(applicationDescription);
+        return jobDescription;
+    }
+
+    private ResourceDescription buildResourceDescription()
+            throws JobSpecificationException {
+        ResourceDescription resourceDescription = new ResourceDescription();
+        resourceDescription.setHostName(getParameter("hostName").trim());
+        resourceDescription.setJobScheduler(getParameter("jobScheduler").trim());
+        resourceDescription.setMemory(getParameterAsLng("memory"));
+        resourceDescription.setCpuCount(getParameterAsLng("cpuCount"));
+        resourceDescription.setCpuSpeed(getParameterAsLng("cpuSpeed"));
+        resourceDescription.setOsType(getParameter("osType").trim());
+        resourceDescription.setOsName(getParameter("osName").trim());
+        resourceDescription.setOsVersion(getParameter("osVersion").trim());
+        resourceDescription.setOsRelease(getParameter("osRelease").trim());
+        return resourceDescription;
+    }
+
+    private ApplicationDescription buildApplicationDescription()
+            throws JobSpecificationException, MalformedURLException {
+        ApplicationDescription applicationDescription = new ApplicationDescription();
+        applicationDescription = new ApplicationDescription();
+        String executable = getParameter("executable").trim();
+        if (executable.length() == 0) {
+            String message = "No Executable specified for application!";
+            throw new JobSpecificationException(message);
+        }
+        applicationDescription.setExecutable(executable);
+        applicationDescription.setStdout(getParameter("stdout").trim());
+        applicationDescription.setStderr(getParameter("stderr").trim());
+        Arguments arguments = buildArguments();
+        applicationDescription.setArguments(arguments);
+        Environment environment = buildEnvironment();
+        applicationDescription.setEnvironment(environment);
+        return applicationDescription;
+    }
+
+    private Arguments buildArguments() {
+        Arguments arguments = new Arguments();
+        String argumentsText = getParameter("arguments");
+        if (argumentsText != null) {
+            StringTokenizer argumentTokens = new StringTokenizer(argumentsText);
+            while (argumentTokens.hasMoreTokens()) {
+                String value = argumentTokens.nextToken();
+                Argument argument = new Argument();
+                argument.setValue(value);
+                arguments.addArgument(argument);
+            }
+        }
+        return arguments;
+    }
+
+    private Environment buildEnvironment()
+            throws JobSpecificationException {
+        Environment environment = new Environment();
+        String environmentText = getParameter("environment");
+        if (environmentText != null) {
+            StringTokenizer environmentTokens = new StringTokenizer(environmentText);
+            while (environmentTokens.hasMoreTokens()) {
+                String variableText = environmentTokens.nextToken();
+                StringTokenizer variableTokens = new StringTokenizer(variableText, "=");
+                if (variableTokens.countTokens() == 0) {
+                    String message = "Environment variable ["
+                            + variableText
+                            + "] not properly assigned!";
+                    throw new JobSpecificationException(message);
+                } else {
+                    String variableName = variableTokens.nextToken();
+                    String variableValue = variableTokens.nextToken();
+                    EnvironmentVariable variable = new EnvironmentVariable();
+                    variable.setName(variableName);
+                    variable.setValue(variableValue);
+                    environment.putVariable(variable);
+                }
+            }
+        }
+        return environment;
+    }
+}
