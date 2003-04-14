c0413abeecc61143215ebfcb8aa4fbc92c8b34a8
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2003-04-14 08:50:11 +0000

    new portlet-error-frmae used to disaply errors
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@1502 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/layout/PortletErrorFrame.java
@@ -0,0 +1,74 @@
+package org.gridlab.gridsphere.layout;
+
+import org.gridlab.gridsphere.portletcontainer.GridSphereEvent;
+import org.gridlab.gridsphere.portletcontainer.GridSphereProperties;
+import org.gridlab.gridsphere.portlet.PortletException;
+import org.gridlab.gridsphere.portlet.PortletRequest;
+import org.gridlab.gridsphere.portlet.PortletResponse;
+
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * @author <a href="mailto:novotny@aei.mpg.de">Jason Novotny</a>
+ * @version $Id$
+ */
+public class PortletErrorFrame extends BasePortletComponent implements Cloneable, Serializable {
+
+    private String message = null;
+    private Exception cause = null;
+
+    public PortletErrorFrame() {
+
+    }
+
+    public boolean hasError() {
+        return ((message == null) && (cause == null)) ? false : true;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setException(PortletException cause) {
+        this.cause = cause;
+    }
+
+    public void setError(String message, Exception cause) {
+        this.message = message;
+        this.cause = cause;
+    }
+
+    public void doRender(GridSphereEvent event) throws PortletLayoutException, IOException {
+        super.doRender(event);
+        PortletRequest req = event.getPortletRequest();
+        PortletResponse res = event.getPortletResponse();
+        PrintWriter out = res.getWriter();
+        PortletException portletException = (PortletException)req.getAttribute(GridSphereProperties.ERROR);
+        if (portletException == null) {
+            if (message != null) {
+                out.println(message);
+                message = null;
+            }
+            if (cause != null) {
+                cause.printStackTrace(out);
+                cause = null;
+            }
+        } else {
+            out.println(portletException.getMessage() + "\n");
+            out.println("<br>");
+            portletException.printStackTrace(out);
+        }
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+        PortletErrorFrame f = (PortletErrorFrame)super.clone();
+        f.message = this.message;
+        return f;
+    }
+}
