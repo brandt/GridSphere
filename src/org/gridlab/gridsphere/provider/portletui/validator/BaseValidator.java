400054aa6eb45508062344bc621fd6961ce5c575
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2003-06-19 19:06:19 +0000

    *** empty log message ***
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@1753 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/provider/portletui/validator/BaseValidator.java
@@ -0,0 +1,13 @@
+/*
+ * @author <a href="mailto:novotny@aei.mpg.de">Jason Novotny</a>
+ * @version $Id$
+ */
+package org.gridlab.gridsphere.provider.portletui.validator;
+
+public class BaseValidator implements Validator {
+
+    public boolean isValid() {
+        return true;
+    }
+
+}
