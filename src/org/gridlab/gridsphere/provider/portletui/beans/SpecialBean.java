c17135e5495a5872afc5ce81d320b383c2a04b35
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2003-07-10 16:29:33 +0000

    example of demonstarting a bean for javascript
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@1805 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/provider/portletui/beans/SpecialBean.java
@@ -0,0 +1,46 @@
+/**
+ * @author <a href="oliver.wehrens@aei.mpg.de">Oliver Wehrens</a>
+ * @version $Id$
+ */
+package org.gridlab.gridsphere.provider.portletui.beans;
+
+import org.gridlab.gridsphere.portlet.PortletRequest;
+
+public class SpecialBean extends BaseComponentBean implements TagBean  {
+
+    public static final String NAME = "sp";
+
+    public static final String TEXTFIELD_STYLE = "portlet-frame-text";
+
+    public SpecialBean() {
+        super(NAME);
+        this.cssStyle = TEXTFIELD_STYLE;
+    }
+
+    public SpecialBean(String name) {
+        super(name);
+        this.cssStyle = TEXTFIELD_STYLE;
+    }
+
+    public SpecialBean(String name, String id) {
+        super(name);
+        this.beanId = id;
+        this.cssStyle = TEXTFIELD_STYLE;
+    }
+
+     public SpecialBean(PortletRequest request, String id) {
+        super(NAME);
+        this.cssStyle = TEXTFIELD_STYLE;
+        this.request = request;
+        this.beanId = id;
+    }
+
+    public String toString() {
+        if (supportsJS) {
+            return "<b>a javascript interface</b>";
+        } else {
+            return "<b>a non-javascript interface</b>";
+        }
+    }
+
+}
