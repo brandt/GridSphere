a210485752639858d3265ffeffc0db8607073fa6
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2003-08-20 14:10:18 +0000

    added base interface descriptions for a component event and action
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@1894 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/layout/event/PortletComponentEvent.java
@@ -0,0 +1,36 @@
+/*
+ * @author <a href="mailto:novotny@aei.mpg.de">Jason Novotny</a>
+ * @version $Id$
+ */
+package org.gridlab.gridsphere.layout.event;
+
+import org.gridlab.gridsphere.layout.PortletTab;
+import org.gridlab.gridsphere.layout.PortletComponent;
+
+/**
+ * A <code>PortletComponentEvent</code> is a general portlet render event
+ */
+public interface PortletComponentEvent {
+
+    /**
+     * Returns the portlet title bar event action
+     *
+     * @return the portlet title bar event action
+     */
+    public ComponentAction getAction();
+
+    /**
+     * Returns the PortletComponent that was selected
+     *
+     * @return the PortletComponent that was selcted
+     */
+    public PortletComponent getPortletComponent();
+
+    /**
+     * Returns the component id of the portlet component
+     *
+     * @return the component id of the portlet component
+     */
+    public int getID();
+
+}
