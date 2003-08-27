d671c697d16d752ff72cf17b4d36bf91341f7bf4
wehrens <wehrens@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2003-08-27 09:28:53 +0000

    new row layout
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@1964 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/layout/PortletRowLayout.java
@@ -0,0 +1,49 @@
+/*
+ * @author <a href="mailto:oliver.wehrens@aei.mpg.de">Oliver Wehrens</a>
+ * @version $Id$
+ */
+package org.gridlab.gridsphere.layout;
+
+import org.gridlab.gridsphere.portletcontainer.GridSphereEvent;
+import org.gridlab.gridsphere.portlet.PortletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ */
+public class PortletRowLayout extends PortletFrameLayout implements Cloneable {
+
+    public void doRender(GridSphereEvent event) throws PortletLayoutException, IOException {
+        PortletResponse res = event.getPortletResponse();
+        PrintWriter out = res.getWriter();
+
+        PortletComponent p = null;
+
+        // starting of the gridtable
+        out.println(" <!-- START ROW --> ");
+        out.println("<table width=\"100%\" cellspacing=\"0\" cellpadding=\"0\">");
+        out.println("<tbody><tr>") ;
+        for (int i=0;i<components.size();i++) {
+            p = (PortletComponent) components.get(i);
+            System.out.println("\n\n\n\n\n WIDTH "+p.getWidth()+"\n\n\n\n");
+            out.println("<td valign=\"top\" width=\""+p.getWidth()+"\">");
+            if (p.getVisible()) {
+                p.doRender(event);
+            }
+            out.println("</td>");
+        }
+        out.println("</tr></tbody></table>");
+        out.println("<!-- END ROW -->");
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+        PortletRowLayout g = (PortletRowLayout)super.clone();
+        g.width = this.width;
+        return g;
+    }
+}
+ 
+
+
