41adb705bec5ab5823f22382e956356a70d29e86
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2003-08-29 21:19:51 +0000

    fixed session handling notification and logging
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@2022 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

--- src/org/gridlab/gridsphere/services/core/user/UserSessionManager.java
+++ src/org/gridlab/gridsphere/services/core/user/UserSessionManager.java
@@ -46,9 +46,13 @@ public class UserSessionManager implements PortletSessionListener {
         Iterator it = userSessions.keySet().iterator();
         while (it.hasNext()) {
             String userId = (String)it.next();
-            PortletSession s = (PortletSession)userSessions.get(userId);
-            if (s.getId().equals(session.getId())) {
-                return userId;
+            List portletSessions = (List)userSessions.get(userId);
+            Iterator pit = portletSessions.iterator();
+            while(pit.hasNext()) {
+                PortletSession s = (PortletSession)pit.next();
+                if (s.getId().equals(session.getId())) {
+                    return userId;
+                }
             }
         }
         return null;
@@ -63,6 +67,7 @@ public class UserSessionManager implements PortletSessionListener {
         return l;
     }
 
+    /*
     public List getSessions() {
         List l = new ArrayList();
         Iterator it = userSessions.values().iterator();
@@ -71,60 +76,74 @@ public class UserSessionManager implements PortletSessionListener {
         }
         return l;
     }
+    */
 
     public void login(PortletRequest request) {
         // so far do nothing
     }
 
     public void logout(PortletSession session) {
-        log.debug("before logout");
-        dumpSessions();
-        if (userSessions.containsValue(session)) {
-            Iterator it = userSessions.keySet().iterator();
-            while (it.hasNext()) {
-                User u = (User)it.next();
-                PortletSession s = (PortletSession)userSessions.get(u.getID());
-                if (s.getId().equals(session.getId())) {
-                    userSessions.remove(u.getID());
-                    break;
-                }
+        log.debug("in logout");
+        Set s = userSessions.keySet();
+        Iterator it = s.iterator();
+        while (it.hasNext()) {
+            String uid = (String)it.next();
+            List sessions = (List)userSessions.get(uid);
+            log.debug("checking if user " + uid + " has session" + session.getId());
+            if (sessions.contains(session)) {
+                log.debug("Logging out user: " +  uid + " session: " + session.getId());
+                sessions.remove(session);
             }
         }
-        log.debug("after logout");
         dumpSessions();
     }
 
     public void removeSessions(User user) {
-        log.error("Removing session for user " + user.getID());
-        List userSessions = getSessions(user);
-        if (userSessions != null) {
-            Iterator it = userSessions.iterator();
+        log.debug("Removing session for user " + user.getID());
+        //List userSessions = (List)userSessions.get(user.getID());
+        List s = getSessions(user);
+        if (s != null) {
+            Iterator it = s.iterator();
             while (it.hasNext()) {
                 PortletSession session = (PortletSession)it.next();
                 if (session != null) {
                     try {
+                        log.debug("invalidating session " + session.getId());
                         session.invalidate();
                     } catch (IllegalStateException e) {
                         log.debug("session " + session.getId() + " for user " + user.getID() + " has already been invalidated!");
                     }
                 }
             }
+            userSessions.remove(user.getID());
         }
+
+        /*
         if (user != null) {
-            if (user.getID() != null) userSessions.remove(user.getID());
+            if (user.getID() != null) {
+                log.debug("removing " + user.getID() + " from user session manager");
+                userSessions.remove(user.getID());
+            }
         }
+        */
     }
 
     public void dumpSessions() {
-        log.info("Session information:");
+        log.info("User Session Manager information:");
         log.info("# current sessions: " + userSessions.size());
         Set keySet = userSessions.keySet();
         Iterator it = keySet.iterator();
         while (it.hasNext()) {
             String u = (String)it.next();
-            PortletSession s = (PortletSession)userSessions.get(u);
+            List l = (List)userSessions.get(u);
+            Iterator lit = l.iterator();
+            while (lit.hasNext()) {
+                PortletSession s = (PortletSession)lit.next();
             log.info("User: " + u + " has session id: " + s.getId());
+            }
 
         }
+
+        sessionManager.dumpSessions();
     }
 }
