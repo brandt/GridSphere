a65f1ceb0afaf3c362ede6bfda9e430d1a4b2ca4
kisg <kisg@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2003-12-31 14:17:54 +0000

    Automatic localization support for PortletContent. For example  instead /html/pagehead.html /html/pagehead-$locale.html will be loaded, if present.
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@2487 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

--- src/org/gridlab/gridsphere/layout/PortletContent.java
+++ src/org/gridlab/gridsphere/layout/PortletContent.java
@@ -4,14 +4,16 @@
  */
 package org.gridlab.gridsphere.layout;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+
 import org.gridlab.gridsphere.portlet.PortletContext;
 import org.gridlab.gridsphere.portlet.PortletException;
 import org.gridlab.gridsphere.portlet.PortletRequest;
 import org.gridlab.gridsphere.portlet.PortletResponse;
+import org.gridlab.gridsphere.portlet.User;
 import org.gridlab.gridsphere.portletcontainer.GridSphereEvent;
-
-import java.io.IOException;
-import java.io.Serializable;
 
 /**
  * <code>PortletContent</code> is used to display the contents of an included
@@ -59,7 +61,48 @@ public class PortletContent extends BasePortletComponent implements Serializable
         PortletRequest req = event.getPortletRequest();
         PortletResponse res = event.getPortletResponse();
         if (textFile != null) {
+
+            // Try the localized version first
             try {
+                int dotIndex=textFile.lastIndexOf(".");
+                String textFilePrefix = textFile.substring(0,dotIndex);
+                String textFileSuffix = textFile.substring(dotIndex);
+                
+                String locale = (String) req.getPortletSession(true).getAttribute(User.LOCALE);
+                
+                if (locale != null) {       
+                        // This locFile hack is needed because ctx.include does not throw an exception if localizedTextFile 
+                        // doesn't exists.
+                        File locFile = null;
+                        
+                        String localizedTextFile = textFilePrefix + "-" + locale + textFileSuffix;
+                        
+                        locFile = new File(ctx.getRealPath(localizedTextFile));
+                        
+                        if (!locFile.isFile()) {
+                                // try without the _XY part of the xy_XY type locales.
+                                int index = locale.indexOf("_");
+                                if (index >= 0) {
+                                        locale = locale.substring(0,index);
+                                }
+                               
+                                localizedTextFile = textFilePrefix + "-" + locale + textFileSuffix;
+                                
+                                locFile = new File(ctx.getRealPath(localizedTextFile));
+                                if (locFile.isFile()) {
+                                        ctx.include(localizedTextFile, req, res);    
+                                        return ;                    
+                                }                                
+                        } else {
+                                ctx.include(localizedTextFile, req, res);    
+                                return ;                    
+                        }
+                }                                     
+            } catch (PortletException e) {
+                    // Ignore, because localization is optional.
+            }
+
+            try {                    
                 ctx.include(textFile, req, res);
             } catch (PortletException e) {
                 throw new PortletLayoutException("Unable to include text: " + textFile, e);
