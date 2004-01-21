35f2c983654841038622f748acea37670a1f2f16
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2004-01-21 01:24:15 +0000

    made fix provided by Guillaume
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@2573 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

--- src/org/gridlab/gridsphere/services/core/file/impl/FileManagerServiceImpl.java
+++ src/org/gridlab/gridsphere/services/core/file/impl/FileManagerServiceImpl.java
@@ -44,6 +44,7 @@ public class FileManagerServiceImpl implements FileManagerService, PortletServic
 
 
     public String getLocationPath(User user, String fileName) {
+        fileName = fileName.substring(fileName.lastIndexOf(File.separator)+1);
         String userLoc = PORTAL_TMP_DIR + File.separator + user.getID() + File.separator + fileName;
         //String fileLoc = userLoc + File.separator + fileName;
         //File userDir = new File(userLoc);
