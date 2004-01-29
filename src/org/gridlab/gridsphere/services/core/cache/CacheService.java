e6e8128e9133eaa844109c9dee03dab3c9ff65c4
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2004-01-29 00:09:54 +0000

    added Gergley's cache and timer service to core
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@2606 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/services/core/cache/CacheService.java
@@ -0,0 +1,22 @@
+/*
+ * @author <a href="mailto:kisg@mailbox.hu">Gergely Kis</a>
+ * @author <a href="mailto:novotny@aei.mpg.de">Jason Novotny</a>
+ * @version $Id$
+ */
+package org.gridlab.gridsphere.services.core.cache;
+
+import java.util.Date;
+
+/**
+ * Simple Cache service. Caches java objects with a String key and a timeout.
+ */
+public interface CacheService {
+
+    void cache(String key, Object object, long timeout, boolean rolling);
+
+    void cache(String key, Object object, Date timeout);
+
+    void removeCached(String key);
+
+    Object getCached(String key);
+}
---
+++ src/org/gridlab/gridsphere/services/core/cache/impl/CacheServiceImpl.java
@@ -0,0 +1,129 @@
+/**
+ * @author <a href="mailto:kisg@mailbox.hu">Gergely Kis</a>
+ * @author <a href="mailto:novotny@aei.mpg.de">Jason Novotny</a>
+ * @version $Id$
+ */
+package org.gridlab.gridsphere.services.core.cache.impl;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TimerTask;
+
+import org.gridlab.gridsphere.portlet.service.PortletServiceNotFoundException;
+import org.gridlab.gridsphere.portlet.service.PortletServiceUnavailableException;
+import org.gridlab.gridsphere.portlet.service.spi.PortletServiceConfig;
+import org.gridlab.gridsphere.portlet.service.spi.PortletServiceFactory;
+import org.gridlab.gridsphere.portlet.service.spi.PortletServiceProvider;
+import org.gridlab.gridsphere.portlet.service.spi.impl.SportletServiceFactory;
+import org.gridlab.gridsphere.services.core.cache.CacheService;
+import org.gridlab.gridsphere.services.core.timer.TimerService;
+
+/**
+ * Simple CacheService implementation based on TimerService
+ */
+public class CacheServiceImpl implements PortletServiceProvider, CacheService {
+
+    /**
+     * Erases the timed out objects from the cache.
+     */
+    private static class CacheServiceCleaner extends TimerTask {
+
+        CacheServiceImpl impl;
+        public void run() {
+            impl.clearExpiredEntries();
+        }
+
+        /**
+         * @param impl
+         */
+        public CacheServiceCleaner(CacheServiceImpl impl) {
+            this.impl = impl;
+        }
+
+    }
+
+    private static class CacheObject {
+        public String key;
+        public boolean rolling;
+        public long expiration;
+        public long delay;
+        public Object cached;
+
+        public CacheObject(String key, Object cached, long delay, boolean rolling) {
+            this.key = key;
+            this.cached = cached;
+            this.delay = delay;
+            this.expiration = System.currentTimeMillis() + delay;
+        }
+
+        public void rollExpiration() {
+            if (rolling) {
+                this.expiration = System.currentTimeMillis() + delay;
+            }
+        }
+    }
+
+    TimerService timerService;
+    Map key2object;
+
+
+    public void init(PortletServiceConfig config) throws PortletServiceUnavailableException {
+        PortletServiceFactory factory = SportletServiceFactory.getInstance();
+        try {
+            timerService = (TimerService) factory.createPortletService(TimerService.class, config.getServletContext(), true);
+            timerService.schedule("org.gridlab.gridslide.service.CacheService", new CacheServiceCleaner(this),1000,60000);
+        } catch (PortletServiceUnavailableException e) {
+            throw new PortletServiceUnavailableException("Cannot initialize CacheManager service", e);
+        } catch (PortletServiceNotFoundException e) {
+            throw new PortletServiceUnavailableException("Cannot initialize CacheManager service", e);
+        }
+        key2object = new HashMap();
+    }
+
+    public void destroy() {
+        key2object.clear();
+        timerService.cancel("org.gridlab.gridslide.service.CacheService");
+    }
+
+    public synchronized void cache(String key, Object object, long timeout, boolean rolling) {
+        key2object.put(key.intern(), new CacheObject(key.intern(), object, timeout, rolling));
+    }
+
+    public synchronized void cache(String key, Object object, Date timeout) {
+        key2object.put(key.intern(), new CacheObject(key.intern(), object, timeout.getTime(), false));
+    }
+
+    public synchronized Object getCached(String key) {
+        CacheObject cobj = (CacheObject) key2object.get(key.intern());
+        if (cobj != null) {
+            cobj.rollExpiration();
+            return cobj.cached;
+        }
+        return null;
+    }
+
+    protected synchronized void clearExpiredEntries() {
+        Set expiredKeys = new HashSet();
+        Iterator cacheIter = key2object.values().iterator();
+        long currentTime = System.currentTimeMillis();
+        while (cacheIter.hasNext()) {
+            CacheObject cobj = (CacheObject) cacheIter.next();
+            if ( cobj.delay > 0 && cobj.expiration < currentTime) {
+                expiredKeys.add(cobj.key);
+            }
+        }
+        Iterator keyIter = expiredKeys.iterator();
+        while (keyIter.hasNext()) {
+            key2object.remove(keyIter.next());
+        }
+    }
+
+    public synchronized void removeCached(String key) {
+        key2object.remove(key.intern());
+    }
+
+}
