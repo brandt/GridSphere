cbe158c31764439109c9d42659a6c3c4902431a9
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2004-02-20 13:36:23 +0000

    constructor only requires HttpServletResponse not PortletResponse
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@2730 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

--- src/org/gridlab/gridsphere/portlet/impl/StoredPortletResponseImpl.java
+++ src/org/gridlab/gridsphere/portlet/impl/StoredPortletResponseImpl.java
@@ -9,15 +9,16 @@ import org.gridlab.gridsphere.portlet.PortletResponse;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.Writer;
 
 public class StoredPortletResponseImpl extends SportletResponse
 {
     private PrintWriter writer;
 
-    public StoredPortletResponseImpl(HttpServletResponse response, PrintWriter writer)
+    public StoredPortletResponseImpl(HttpServletResponse response, Writer writer)
     {
         super(response);
-        this.writer = writer;
+        this.writer = new PrintWriter(writer);
     }
 
     public PrintWriter getWriter()
