a14fe6fab1d87f8fff3eb3d2b04b813923eb926c
docentt <docentt@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2004-03-11 23:57:15 +0000

    Secure Directory Service - new file
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@2789 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/services/core/secdir/SecureDirectoryService.java
@@ -0,0 +1,22 @@
+/**
+ * @author <a href="mailto:tkucz@icis.pcz.pl">Tomasz Kuczynski</a>
+ * @version 0.1 2004/03/11
+ */
+package org.gridlab.gridsphere.services.core.secdir;
+
+import org.gridlab.gridsphere.portlet.User;
+import org.gridlab.gridsphere.portlet.service.PortletService;
+
+import java.io.File;
+import java.io.InputStream; 
+
+public interface SecureDirectoryService extends PortletService{
+  public String getFileUrl(User user,String appName,String resource);
+	public String getFileUrl(User user,String appName,String resource,String saveAs);
+	public String getFileUrl(User user,String appName,String resource,String saveAs,String contentType);
+  public File getFile(User user,String appName,String resource);
+  public boolean writeFromStream(User user,String appName,String resource,InputStream input);
+  public boolean writeFromFile(User user,String appName,String resource,File inputFile);
+  public boolean deleteFile(User user,String appName,String resource);
+  public boolean fileExists(User user,String appName,String resource);
+}
