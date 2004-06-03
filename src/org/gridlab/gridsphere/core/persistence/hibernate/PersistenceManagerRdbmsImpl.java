d58a0056201ce55cb0db6eb3ec8a14342e7f8446
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2004-06-03 10:26:59 +0000

    *** empty log message ***
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@3087 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

--- src/org/gridlab/gridsphere/core/persistence/hibernate/PersistenceManagerRdbmsImpl.java
+++ src/org/gridlab/gridsphere/core/persistence/hibernate/PersistenceManagerRdbmsImpl.java
@@ -8,7 +8,6 @@ import net.sf.hibernate.*;
 import net.sf.hibernate.cfg.Configuration;
 import org.gridlab.gridsphere.core.persistence.PersistenceManagerException;
 import org.gridlab.gridsphere.core.persistence.PersistenceManagerRdbms;
-import org.gridlab.gridsphere.core.persistence.PersistenceSession;
 import org.gridlab.gridsphere.portlet.PortletLog;
 import org.gridlab.gridsphere.portlet.impl.SportletLog;
 import org.gridlab.gridsphere.portletcontainer.GridSphereConfig;
