5f593935d9a52ef0406db5113beb14dd0f849463
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2004-06-14 14:01:33 +0000

    *** empty log message ***
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@3165 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/provider/portletui/tags/HasRoleTag.java
@@ -0,0 +1,84 @@
+/**
+ * @author <a href="mailto:novotny@aei.mpg.de">Jason Novotny</a>
+ * @version $Id$
+ */
+package org.gridlab.gridsphere.provider.portletui.tags;
+
+import org.gridlab.gridsphere.portlet.PortletRequest;
+import org.gridlab.gridsphere.portlet.PortletRole;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+/**
+ * The <code>HasRoleTag</code> can be used to selectively display presentation based upon a user's role
+ */
+public class HasRoleTag extends TagSupport {
+
+    protected PortletRole role = PortletRole.GUEST;
+    protected boolean exclusive = false;
+
+    /**
+     * Sets the user's role
+     *
+     * @param role the user's role
+     */
+    public void setRole(String role) {
+        try {
+            this.role = PortletRole.toPortletRole(role);
+        } catch (IllegalArgumentException e) {
+            // do nothing
+            this.role = PortletRole.GUEST;
+        }
+    }
+
+    /**
+     * Returns the user's role
+     *
+     * @return the user's role
+     */
+    public String getRole() {
+        return role.toString();
+    }
+
+    /**
+     * If exclusive is set to true then presentation will be displayed ONLY if the user has this exact role
+     *
+     * @param exclusive is true if presentation will be displayed ONLY if the user has this exact role
+     */
+    public void setExclusive(boolean exclusive) {
+        this.exclusive = exclusive;
+    }
+
+    /**
+     * If exclusive is set to true then presentation will be displayed ONLY if the user has this exact role
+     *
+     * @return true if presentation will be displayed ONLY if the user has this exact role
+     */
+    public boolean getExclusive() {
+        return exclusive;
+    }
+
+    public int doStartTag() throws JspException {
+        PortletRequest req = (PortletRequest)pageContext.getAttribute("portletRequest");
+        PortletRole userRole = req.getRole();
+
+        if (userRole != null) {
+            if (exclusive) {
+                if ((userRole.getName()) == role.getName()) {
+                    return EVAL_BODY_INCLUDE;
+                }
+            } else {
+                if ((userRole.getID()) >= role.getID()) {
+                    return EVAL_BODY_INCLUDE;
+                }
+            }
+        }
+        return SKIP_BODY;
+    }
+
+    public int doEndTag() throws JspException {
+        return EVAL_PAGE;
+    }
+
+}
---
+++ src/org/gridlab/gridsphere/services/core/portal/impl/PortalConfigServiceImpl.java
@@ -0,0 +1,76 @@
+/**
+ * @author <a href="mailto:kisg@mailbox.hu">Gergely Kis</a>
+ * @author <a href="mailto:novotny@aei.mpg.de">Jason Novotny</a>
+ * @version $Id$
+ */
+package org.gridlab.gridsphere.services.core.portal.impl;
+
+import org.gridlab.gridsphere.portlet.service.PortletServiceUnavailableException;
+import org.gridlab.gridsphere.portlet.service.spi.PortletServiceConfig;
+import org.gridlab.gridsphere.portlet.service.spi.PortletServiceProvider;
+import org.gridlab.gridsphere.portlet.impl.SportletLog;
+import org.gridlab.gridsphere.portlet.impl.SportletGroup;
+import org.gridlab.gridsphere.portlet.PortletLog;
+import org.gridlab.gridsphere.services.core.portal.PortalConfigService;
+import org.gridlab.gridsphere.services.core.portal.PortalConfigSettings;
+import org.gridlab.gridsphere.core.persistence.PersistenceManagerRdbms;
+import org.gridlab.gridsphere.core.persistence.PersistenceManagerFactory;
+import org.gridlab.gridsphere.core.persistence.PersistenceManagerException;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Portal configuration service is used to manage portal administrative settings
+ */
+public class PortalConfigServiceImpl implements PortletServiceProvider, PortalConfigService {
+
+    private PortletLog log = SportletLog.getInstance(PortalConfigServiceImpl.class);
+
+    private PersistenceManagerRdbms pm = PersistenceManagerFactory.createGridSphereRdbms();
+
+    public synchronized void init(PortletServiceConfig config) throws PortletServiceUnavailableException {
+
+        PortalConfigSettings configSettings = this.getPortalConfigSettings();
+        if (configSettings == null) {
+            configSettings = new PortalConfigSettings();
+            boolean canCreate = Boolean.getBoolean(config.getInitParameter("canUserCreateNewAccount"));
+            configSettings.setCanUserCreateAccount(canCreate);
+            // set gridsphere as a default group
+            Set defaultGroups = new HashSet();
+            defaultGroups.add(SportletGroup.CORE);
+            configSettings.setDefaultGroups(defaultGroups);
+            // set default theme
+            configSettings.setDefaultTheme("default");
+            savePortalConfigSettings(configSettings);
+        }
+
+    }
+
+    public synchronized void destroy() {
+
+    }
+
+    public void savePortalConfigSettings(PortalConfigSettings configSettings) {
+        try {
+            if (configSettings.getOid() == null) {
+                pm.create(configSettings);
+            } else {
+                pm.update(configSettings);
+            }
+        } catch (PersistenceManagerException e) {
+            log.error("Unable to save or update config settings!", e);
+        }
+    }
+
+    public PortalConfigSettings getPortalConfigSettings() {
+        PortalConfigSettings settings = null;
+        try {
+            settings = (PortalConfigSettings)pm.restore("select c from " + PortalConfigSettings.class.getName() + " c");
+        } catch (PersistenceManagerException e) {
+            log.error("Unable to retrieve config settings!", e);
+        }
+        return settings;
+    }
+
+}
