4a5b1dabc1dd6e4dc22a54a69c80704e2285198f
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2004-09-01 14:40:08 +0000

    *** empty log message ***
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@3539 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

--- src/org/gridlab/gridsphere/portletcontainer/GridSphereConfig.java
+++ src/org/gridlab/gridsphere/portletcontainer/GridSphereConfig.java
@@ -7,6 +7,7 @@ package org.gridlab.gridsphere.portletcontainer;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import java.util.ResourceBundle;
+import java.io.*;
 
 /**
  * <code>GridSphereConfig</code> represents the <code>gridsphere.properties</code> properties file
@@ -16,6 +17,7 @@ public class GridSphereConfig implements GridSphereConfigProperties {
 
     public static final String pathtype = System.getProperty("file.separator");
     protected static ServletConfig config = null;
+    protected static ServletContext context = null;
     protected static ResourceBundle configBundle = null;
     protected static String contextPath = "";
     protected static String servletPath = "";
@@ -24,6 +26,7 @@ public class GridSphereConfig implements GridSphereConfigProperties {
 
     public static void setServletConfig(ServletConfig servletConfig) {
         config = servletConfig;
+        context = config.getServletContext();
     }
 
     public static ServletConfig getServletConfig() {
@@ -31,7 +34,25 @@ public class GridSphereConfig implements GridSphereConfigProperties {
     }
 
     public static ServletContext getServletContext() {
-        return config.getServletContext();
+        return context;
+    }
+
+    public static void setServletContext(ServletContext ctx) {
+        context = ctx;
+    }
+
+    public static void copyFile(File src, File dst) throws IOException {
+        InputStream in = new FileInputStream(src);
+        OutputStream out = new FileOutputStream(dst);
+
+        // Transfer bytes from in to out
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = in.read(buf)) > 0) {
+            out.write(buf, 0, len);
+        }
+        in.close();
+        out.close();
     }
 
     /**
