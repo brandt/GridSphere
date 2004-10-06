f1011934727ed0fdc7c876c8d14bdadf1859475c
brango <brango@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2004-10-06 09:25:04 +0000

    DatePortlet.java   VS: ----------------------------------------------------------------------
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@3620 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/portlets/core/date/DatePortlet.java
@@ -0,0 +1,45 @@
+
+package org.gridlab.gridsphere.portlets.core.date;
+
+import org.gridlab.gridsphere.portlet.*;
+import org.gridlab.gridsphere.portlet.PortletException;
+import org.gridlab.gridsphere.portlet.PortletRequest;
+import org.gridlab.gridsphere.portlet.PortletResponse;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.TimeZone;
+import java.util.Locale;
+import java.text.DateFormat;
+
+
+public class DatePortlet extends AbstractPortlet {
+
+    public void doView(PortletRequest request, PortletResponse response) throws PortletException, IOException {
+
+            Locale locale = null;
+            TimeZone tz = null;
+            User user = request.getUser();
+            String tzStr = (String)user.getAttribute(User.TIMEZONE);
+            locale = request.getLocale();
+          if(request.getRole().toString().equals("GUEST")){
+            tz = TimeZone.getDefault() ;
+            locale = Locale.getDefault();
+            }
+          else{
+          if (tzStr == null) {
+                 tz = TimeZone.getDefault();
+            } else {
+                 tz = TimeZone.getTimeZone(tzStr);
+           }
+           if (locale==null) {
+              locale = Locale.getDefault();
+            }
+          }
+         Calendar date = Calendar.getInstance(tz, locale);
+         DateFormat uformater = DateFormat.getDateInstance(DateFormat.LONG, locale);
+         uformater.setCalendar(date);
+         request.setAttribute("date", uformater.format(date.getTime()));
+         getPortletConfig().getContext().include("/jsp/date/date.jsp", request, response);
+    }
+
+}
