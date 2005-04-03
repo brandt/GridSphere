2ef7da92f9a0a85ce2d6d3a0e5bfc79ed7ed51e1
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2005-04-03 21:01:02 +0000

    a few more mods so that portlet links used  in float state set the window state to render only the portlet frames and not entire layout
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@3941 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

--- src/org/gridlab/gridsphere/portlet/impl/SportletProperties.java
+++ src/org/gridlab/gridsphere/portlet/impl/SportletProperties.java
@@ -221,5 +221,7 @@ public interface SportletProperties {
     public static final String RENDER_PARAM_PREFIX = "rp_";
 
     public static final String RENDER_OUTPUT = "org.gridlab.gridsphere.layout.RENDER_OUTPUT.";
+
+    public static final String FLOAT_STATE = "org.gridlab.gridsphere.portlet.FLOAT_STATE";
 }
 
