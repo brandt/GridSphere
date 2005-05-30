f57d423aa8ae8d6ba27ff5949ee419b273bd5cb9
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2005-05-30 23:31:48 +0000

    update to 2.0.3
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@4011 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

--- src/org/gridlab/gridsphere/portletcontainer/GridSphereConfigProperties.java
+++ src/org/gridlab/gridsphere/portletcontainer/GridSphereConfigProperties.java
@@ -13,7 +13,7 @@ public interface GridSphereConfigProperties {
     /**
      * Location of GridSphere directory used to store persistent data
      */
-    public static final String GRIDSPHERE_INFO = "GridSphere/2.0.2";
+    public static final String GRIDSPHERE_INFO = "GridSphere/2.0.3";
 
     /**
      * JSR 168 Portlet API version info
