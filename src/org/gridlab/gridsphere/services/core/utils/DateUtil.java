8c45d2a57d0754691c29fe7a523574d75bf187ce
russell <russell@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2005-07-12 14:26:57 +0000

    *. More useful methods for localizing dates.
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@4082 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

--- src/org/gridlab/gridsphere/services/core/utils/DateUtil.java
+++ src/org/gridlab/gridsphere/services/core/utils/DateUtil.java
@@ -2,6 +2,8 @@ package org.gridlab.gridsphere.services.core.utils;
 
 import org.gridlab.gridsphere.portlet.User;
 
+import javax.portlet.PortletRequest;
+import javax.servlet.http.HttpServletRequest;
 import java.text.DateFormat;
 import java.util.*;
 
@@ -12,6 +14,176 @@ import java.util.*;
  */
 
 public class DateUtil {
+
+    /**
+     * Converts the given unix time stamp string to java time.
+     * @param timeStamp The time stamp string
+     * @return The resulting java date
+     */
+    public static long unixTimeStampToJavaTime(String timeStamp) {
+        if (timeStamp == null || timeStamp.equals("")) return 0;
+        return (long)(Double.parseDouble(timeStamp) * 1000);
+    }
+
+    /**
+     * Returns a localized date for the given time using the "user.timezone" attribute and the locale stored in the request.
+     * Displays the date and time formats using the <code>DateFormat.MEDIUM</code> format.
+     * @param request The portlet request
+     * @param milisec The time to display
+     * @return The resulting date string
+     * @see DateFormat
+     */
+    public static String getLocalizedDate(PortletRequest request, long milisec) {
+
+        Locale locale = request.getLocale();
+
+        TimeZone tz = getTimeZone(request);
+
+        return getLocalizedDate(locale, tz, milisec, DateFormat.MEDIUM, DateFormat.MEDIUM);
+    }
+
+    /**
+     * Returns a localized date for the given time using the "user.timezone" attribute and the locale stored in the request.
+     * @param request The portlet request
+     * @param milisec The time to display
+     * @param dateFormat The date format
+     * @param timeFormat  The time format
+     * @return The resulting date string
+     * @see DateFormat
+     */
+    public static String getLocalizedDate(PortletRequest request, long milisec, int dateFormat, int timeFormat) {
+
+        Locale locale = request.getLocale();
+
+        TimeZone tz = getTimeZone(request);
+
+        return getLocalizedDate(locale, tz, milisec, dateFormat, timeFormat);
+    }
+
+    /**
+     * Returns a localized date for the given time using the "user.timezone" attribute and the locale stored in the request.
+     * Displays the date and time formats using the <code>DateFormat.MEDIUM</code> format.
+     * @param request The portlet request
+     * @param milisec The time to display
+     * @return The resulting date string
+     * @see DateFormat
+     */
+    public static String getLocalizedDate(HttpServletRequest request, long milisec) {
+
+        Locale locale = request.getLocale();
+
+        TimeZone tz = getTimeZone(request);
+
+        return getLocalizedDate(locale, tz, milisec, DateFormat.MEDIUM, DateFormat.MEDIUM);
+    }
+
+    /**
+     * Returns a localized date for the given time using the "user.timezone" attribute and the locale stored in the request.
+     * @param request The portlet request
+     * @param milisec The time to display
+     * @param dateFormat The date format
+     * @param timeFormat  The time format
+     * @return The resulting date string
+     * @see DateFormat
+     */
+    public static String getLocalizedDate(HttpServletRequest request, long milisec, int dateFormat, int timeFormat) {
+
+        Locale locale = request.getLocale();
+
+        TimeZone tz = getTimeZone(request);
+
+        return getLocalizedDate(locale, tz, milisec, dateFormat, timeFormat);
+    }
+
+    /**
+     * Returns a localized date for the given locale and time zone.
+     * Displays the date and time formats using the <code>DateFormat.MEDIUM</code> format.
+     * @param locale The locale
+     * @param tz The time zone
+     * @param milisec The time to display
+     * @return The resulting date string
+     * @see DateFormat
+     */
+    public static String getLocalizedDate(Locale locale, TimeZone tz, long milisec) {
+        return getLocalizedDate(locale, tz, milisec, DateFormat.MEDIUM, DateFormat.MEDIUM);
+    }
+
+
+    /**
+     * Returns a localized date for the given locale and time zone.
+     * Displays the date and time formats using the <code>DateFormat.MEDIUM</code> format.
+     * @param locale The locale
+     * @param tz The time zone
+     * @param milisec The time to display
+     * @param dateFormat The date format
+     * @param timeFormat  The time format
+     * @return The resulting date string
+     * @see DateFormat
+     */
+    public static String getLocalizedDate(Locale locale, TimeZone tz, long milisec, int dateFormat, int timeFormat) {
+
+        if (milisec == 0) {
+            return "";
+        }
+
+        if (locale == null) {
+            locale = Locale.getDefault();
+        }
+
+        if (tz == null) {
+            tz = TimeZone.getDefault();
+        }
+
+        Calendar cal = Calendar.getInstance(tz, locale);
+        cal.setTimeInMillis(milisec);
+
+        DateFormat uformatter = DateFormat.getDateTimeInstance(timeFormat, dateFormat, locale);
+        uformatter.setCalendar(cal);
+
+        return uformatter.format(cal.getTime());
+    }
+
+    /**
+     * Returns the time zone for the user.timezone portlet attribute in the given porlet request
+     * @param request the portlet request
+     * @return The time zone
+     */
+    public static TimeZone getTimeZone(PortletRequest request) {
+
+        Map userInfo = (Map) request.getAttribute(PortletRequest.USER_INFO);
+        String tzStr = (String) userInfo.get(User.TIMEZONE);
+
+        TimeZone tz = null;
+
+        if (tzStr == null) {
+            tz = TimeZone.getDefault();
+        } else {
+            tz = TimeZone.getTimeZone(tzStr);
+        }
+
+        return tz;
+    }
+
+    /**
+     * Returns the time zone for the user.timezone portlet attribute in the given porlet request
+     * @param request the portlet request
+     * @return The time zone
+     */
+    public static TimeZone getTimeZone(HttpServletRequest request) {
+
+        Map userInfo = (Map) request.getAttribute(PortletRequest.USER_INFO);
+        String tzStr = (String) userInfo.get(User.TIMEZONE);
+
+        TimeZone tz = null;
+
+        if (tzStr == null) {
+            tz = TimeZone.getDefault();
+        } else {
+            tz = TimeZone.getTimeZone(tzStr);
+        }
+
+        return tz;
+    }
 
     /**
      * Returns localized date for the timezone the user is in.
@@ -32,16 +204,8 @@ public class DateUtil {
         } else {
             tz = TimeZone.getTimeZone(tzStr);
         }
-        if (locale == null) {
-            locale = Locale.getDefault();
-        }
-        Calendar cal = Calendar.getInstance(tz, locale);
-        cal.setTimeInMillis(milisec);
 
-        DateFormat uformatter = DateFormat.getDateTimeInstance(timeFormat, dateFormat, locale);
-        uformatter.setCalendar(cal);
-
-        return uformatter.format(cal.getTime());
+        return getLocalizedDate(locale, tz, milisec, dateFormat, timeFormat);
     }
 
 
