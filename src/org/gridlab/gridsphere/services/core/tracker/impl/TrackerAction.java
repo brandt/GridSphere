f1518102ff5108256be60402ebb6022dc0b73444
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2005-08-09 11:30:43 +0000

    pojo for keeping track of actions to track
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@4148 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/services/core/tracker/impl/TrackerAction.java
@@ -0,0 +1,27 @@
+package org.gridlab.gridsphere.services.core.tracker.impl;
+
+
+public class TrackerAction {
+
+    private String oid;
+    private String action;
+
+    public TrackerAction() {}
+
+    public String getOid() {
+        return oid;
+    }
+
+    public void setOid(String oid) {
+        this.oid = oid;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+}
