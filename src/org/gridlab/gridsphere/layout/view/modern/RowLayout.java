80162a896b1c6216693f7e2d9ed940599ce24818
luca <luca@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2005-09-07 18:27:30 +0000

    new modern look and feel view
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@4234 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/layout/view/modern/RowLayout.java
@@ -0,0 +1,45 @@
+/*
+ * @author <a href="mailto:oliver.wehrens@aei.mpg.de">Oliver Wehrens</a>
+ * @version $Id$
+ */
+package org.gridlab.gridsphere.layout.view.modern;
+
+import org.gridlab.gridsphere.layout.PortletComponent;
+import org.gridlab.gridsphere.layout.view.BaseRender;
+import org.gridlab.gridsphere.layout.view.Render;
+import org.gridlab.gridsphere.portletcontainer.GridSphereEvent;
+
+public class RowLayout extends BaseRender implements Render {
+
+    protected static final StringBuffer TOP_ROW =
+            new StringBuffer("\n<!-- START ROW --><div class=\"row\" >");
+
+    protected static final StringBuffer BOTTOM_ROW_BORDER = new StringBuffer("</div>\n");
+
+    protected static final StringBuffer BOTTOM_ROW = new StringBuffer("</div><!-- END ROW -->\n");
+
+    public StringBuffer doStart(GridSphereEvent event, PortletComponent comp) {
+        return TOP_ROW;
+    }
+
+    public StringBuffer doStartBorder(GridSphereEvent event, PortletComponent p) {
+        String temp = "\n<div class=\"column\"";
+        if (!p.getWidth().equals("")) {
+           temp += " style=\"width: " + p.getWidth() + "\""; 
+        }    
+        temp += ">";        
+        return new StringBuffer(temp);       
+    }
+
+    public StringBuffer doEndBorder(GridSphereEvent event, PortletComponent comp) {
+        return BOTTOM_ROW_BORDER;
+    }
+
+    public StringBuffer doEnd(GridSphereEvent event, PortletComponent comp) {
+        return BOTTOM_ROW;
+    }
+
+}
+ 
+
+
