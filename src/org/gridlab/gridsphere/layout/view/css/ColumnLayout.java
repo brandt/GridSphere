7cd01a9e2b0e571e59587e567987c85285d5ef2d
wehrens <wehrens@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2005-12-05 12:40:09 +0000

    refactored modern to css (which makes more sense)
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@4392 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/layout/view/css/ColumnLayout.java
@@ -0,0 +1,44 @@
+/*
+ * @author <a href="mailto:oliver.wehrens@aei.mpg.de">Oliver Wehrens</a>
+ * @version $Id$
+ */
+package org.gridlab.gridsphere.layout.view.css;
+
+import org.gridlab.gridsphere.layout.PortletComponent;
+import org.gridlab.gridsphere.layout.view.BaseRender;
+import org.gridlab.gridsphere.layout.view.Render;
+import org.gridlab.gridsphere.portletcontainer.GridSphereEvent;
+
+
+public class ColumnLayout extends BaseRender implements Render {
+
+    protected static final StringBuffer TOP_COLUMN = new StringBuffer("\n<!-- START COLUMN --><div>");
+    protected static final StringBuffer TOP_COLUMN_BORDER = new StringBuffer("\n<div>");
+    protected static final StringBuffer BOTTOM_COLUMN_BORDER = new StringBuffer("</div>\n");
+    protected static final StringBuffer BOTTOM_COLUMN = new StringBuffer("</div> <!-- END COLUMN -->\n");
+
+    public ColumnLayout() {
+    }
+
+    public StringBuffer doStart(GridSphereEvent event, PortletComponent comp) {
+        return TOP_COLUMN;
+    }
+
+    public StringBuffer doStartBorder(GridSphereEvent event, PortletComponent comp) {
+        return TOP_COLUMN_BORDER;
+    }
+
+    public StringBuffer doEndBorder(GridSphereEvent event, PortletComponent comp) {
+        return BOTTOM_COLUMN_BORDER;
+    }
+
+    public StringBuffer doEnd(GridSphereEvent event, PortletComponent comp) {
+        return BOTTOM_COLUMN;
+    }
+
+
+
+}
+
+
+
