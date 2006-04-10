fcc7d0abbb352acc7a23d01fbdd4785965a5cccd
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2006-04-10 22:17:39 +0000

    use css and not image src for message box icon
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@4745 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

--- src/org/gridlab/gridsphere/provider/portletui/tags/MessageBoxTag.java
+++ src/org/gridlab/gridsphere/provider/portletui/tags/MessageBoxTag.java
@@ -15,7 +15,6 @@ public class MessageBoxTag extends BaseComponentTag {
     protected MessageBoxBean messageBoxBean = null;
     protected String key = null;
     protected String style = null;
-    protected String imagesource = null;
 
     /**
      * Returns the key used to identify localized text
@@ -35,23 +34,6 @@ public class MessageBoxTag extends BaseComponentTag {
         this.key = key;
     }
 
-    /**
-     * Gets the src (url) of the image to be displayed in the messagebox.
-     *
-     * @return URL of the image
-     */
-    public String getImagesource() {
-        return imagesource;
-    }
-
-    /**
-     * Sets the src (url) of the image to be displayed in the messagebox.
-     *
-     * @param imagesource URL of the image to be displayed
-     */
-    public void setImagesource(String imagesource) {
-        this.imagesource = imagesource;
-    }
     /**
      * Sets the style of the text: Available styles are
      * <ul>
@@ -110,9 +92,6 @@ public class MessageBoxTag extends BaseComponentTag {
         }
         if (key != null) {
             messageBoxBean.setValue(getLocalizedText(key));
-        }
-        if (imagesource != null) {
-            messageBoxBean.setImageSource(imagesource);
         }
 
         try {
