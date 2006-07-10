2518d8063fa8a7e8f053006cae79c56f24526b74
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2006-07-10 20:52:59 +0000

    use Yahoo UI ajax libs
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@4916 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ webapps/gridsphere/javascript/gridsphere_ajax.js
@@ -0,0 +1,124 @@
+
+/*
+var callback =
+{
+    success: this.handleSuccess,
+    failure: this.handleFailure,
+    argument: [],
+    scope: this
+}
+*/
+ /*
+function handleSuccess(o) {
+    var cid = o.argument[0];
+    alert(cid);
+    if(o.responseText != undefined){
+        alert(o.responseText);
+        // create a temporary div element to store the responseText...
+        var serverResponse = document.createElement("div");
+        // ... and store the responseText inside it
+        serverResponse.innerHTML = o.responseText;
+        //alert(o.responseText);
+        // create a collection of all the div elements returned
+        tempPageElements = serverResponse.getElementsByTagName("div");
+        // replace existing component with new one
+        document.getElementById(cid).innerHTML = tempPageElements[0].innerHTML;
+    }
+}
+
+function handleFailure(o) {
+    var cid = o.argument[0];
+    if(o.responseText != undefined) {
+        // replace existing component with new one
+        alert(o.responseText);
+        document.getElementById(cid).innerHTML = "Unable to retrieve portlet fragment!!! ";
+    }
+}
+*/
+GridSphereAjaxHandler = {
+
+
+    callback:function() {
+        success = this.handleSuccess;
+        failure = this.handleFailure;
+        argument = [];
+        scope = this;
+    },
+
+    handleSuccess:function(o) {
+        var cid = o.argument[0];
+        alert(cid);
+        if(o.responseText != undefined){
+            alert(o.responseText);
+            // create a temporary div element to store the responseText...
+            var serverResponse = document.createElement("div");
+            // ... and store the responseText inside it
+            serverResponse.innerHTML = o.responseText;
+            //alert(o.responseText);
+            // create a collection of all the div elements returned
+            tempPageElements = serverResponse.getElementsByTagName("div");
+            // replace existing component with new one
+            document.getElementById(cid).innerHTML = tempPageElements[0].innerHTML;
+        }
+    },
+
+    handleFailure:function(o) {
+        var cid = o.argument[0];
+        if(o.responseText != undefined) {
+            // replace existing component with new one
+            alert(o.responseText);
+            document.getElementById(cid).innerHTML = "Unable to retrieve portlet fragment!!! ";
+        }
+    },
+
+    startRequest:function(mycid, action) {
+        // argument formId can be the id or name attribute value of the
+        // HTML form, or an HTML form object.
+        pageElements = document.getElementById(mycid);
+        formElements = pageElements.getElementsByTagName("form");
+
+        YAHOO.util.Connect.setForm(formElements[0]);
+
+        var sUrl = "/gridsphere/gridsphere" + "?ajax=true&cid=" + encodeURIComponent(mycid) + "&" + YAHOO.util.Connect._sFormData;
+        callback.argument = [ mycid ];
+        var postData = null;
+
+        var transaction = YAHOO.util.Connect.asyncRequest('POST', sUrl, callback, postData);
+        //Abort the transaction if it isn't completed in ten seconds.
+        //setTimeout("YAHOO.util.Connect.abort(transaction)",10000);
+    },
+
+    returnPortlet:function(portlet) {
+        // argument formId can be the id or name attribute value of the
+        // HTML form, or an HTML form object.
+        pageElements = document.getElementById("portlet#" + portlet);
+        formElements = pageElements.getElementsByTagName("form");
+        if (formElements) YAHOO.util.Connect.setForm(formElements[0]);
+
+        var sUrl = "/gridsphere/gridsphere" + "?ajax=true&portlet=" + encodeURIComponent(portlet) + "&" + YAHOO.util.Connect._sFormData;
+
+        this.callback.argument = [ "portlet#" + portlet ];
+        var postData = null;
+
+        var transaction = YAHOO.util.Connect.asyncRequest('POST', sUrl, this.callback, postData);
+        //Abort the transaction if it isn't completed in ten seconds.
+        //setTimeout("YAHOO.util.Connect.abort(transaction)",10000);
+    },
+
+    loadPortlets:function() {
+        var portlets = [];
+        var j = 0;
+        divElements = document.getElementsByTagName("div");
+        for (var i = 0; i < divElements.length; i++) {
+            var anid = divElements[i].id;
+            var startIdx = anid.indexOf("portlet");
+            var lastIdx = anid.indexOf('#', startIdx);
+            if (startIdx >= 0) {
+                var portlet = anid.substring(lastIdx+1);
+                var webIdx = anid.indexOf('#', lastIdx);
+                this.returnPortlet(portlet);
+            }
+        }
+    }
+
+};
