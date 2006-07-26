d2a5fc8ef52798a9dfa701f7296a59c8849fe5cb
wehrens <wehrens@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2006-07-26 09:44:58 +0000

    new plaf
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@4950 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/layout/view/brush/TabbedPane.java
@@ -0,0 +1,130 @@
+/*
+* @author <a href="mailto:novotny@aei.mpg.de">Jason Novotny</a>
+* @author <a href="mailto:wehrens@aei.mpg.de">Oliver Wehrens</a>
+* @version $Id: TabbedPane.java 4496 2006-02-08 20:27:04Z wehrens $
+*/
+
+package org.gridlab.gridsphere.layout.view.brush;
+
+import org.gridlab.gridsphere.layout.PortletComponent;
+import org.gridlab.gridsphere.layout.PortletTab;
+import org.gridlab.gridsphere.layout.PortletTabbedPane;
+import org.gridlab.gridsphere.layout.view.BaseRender;
+import org.gridlab.gridsphere.layout.view.TabbedPaneView;
+import org.gridlab.gridsphere.portletcontainer.GridSphereEvent;
+
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class TabbedPane extends BaseRender implements TabbedPaneView {
+
+    protected static final StringBuffer TAB_START_MENU =
+            new StringBuffer("\n<!-- LAYOUT NAVIGATION -->\n <div id=\"gridsphere-layout-navigation\"> \n<ul id=\"gridsphere-nav\"><li id=\"gridsphere-nav-border-left\">&nbsp;</li>\n");
+    protected static final StringBuffer TAB_START_SUBMENU =
+            new StringBuffer("\n<ul id=\"gridsphere-sub-nav\"><li id=\"gridsphere-sub-nav-border-left\">&nbsp;</li>\n");
+
+    protected static final StringBuffer TAB_END_MENU =
+            new StringBuffer("\n<li id=\"gridsphere-nav-border-right\">&nbsp;</li></ul> <!-- END NAVIGATION (SUB) MENU -->\n");
+    protected static final StringBuffer TAB_END_SUBMENU =
+            new StringBuffer("\n<li id=\"gridsphere-sub-nav-border-right\">&nbsp;</li></ul> <!-- END NAVIGATION (SUB) MENU -->\n");
+
+    /**
+     * Constructs an instance of PortletTabbedPane
+     */
+    public TabbedPane() {
+    }
+
+
+    /**
+     * Creates the portlet tab link URIs that are used to send events to
+     * the portlet tabs.
+     *
+     * @param event the gridsphere event
+     */
+    protected static String[] createTabLinks(GridSphereEvent event, PortletTabbedPane pane) {
+        // Make tab links
+        String[] tabLinks = new String[pane.getPortletTabs().size()];
+        List tabs = pane.getPortletTabs();
+        for (int i = 0; i < tabs.size(); i++) {
+            PortletTab tab = (PortletTab) tabs.get(i);
+            tabLinks[i] = tab.createTabTitleLink(event);
+        }
+        return tabLinks;
+    }
+
+    /**
+     * Replace blank spaces in title with '&nbsp;'
+     *
+     * @param title the tab title
+     * @return a title without blank spaces
+     */
+    private static String replaceBlanks(String title) {
+        String result = "&nbsp;";
+        StringTokenizer st = new StringTokenizer(title);
+        while (st.hasMoreTokens()) {
+            result += st.nextToken() + "&nbsp;";
+        }
+        return result;
+    }
+
+    public StringBuffer doStart(GridSphereEvent event, PortletComponent comp) {
+        PortletTabbedPane pane = (PortletTabbedPane) comp;
+        StringBuffer result = new StringBuffer();
+        if (pane.getStyle().equalsIgnoreCase("sub-menu")) {
+            result.append(TAB_START_SUBMENU);
+        }
+        if (pane.getStyle().equals("menu")) {
+            result.append(TAB_START_MENU);
+        }
+
+        return result;
+    }
+
+    public StringBuffer doRenderTab(GridSphereEvent event, PortletTabbedPane tabPane, PortletTab tab) {
+        // this really creates the individual tabs
+        StringBuffer pane = new StringBuffer();
+        String link = tab.createTabTitleLink(event);
+        String lang = event.getPortletRequest().getLocale().getLanguage();
+        String title = tab.getTitle(lang);
+
+        if (tabPane.getStyle().equals("sub-menu")) {
+            pane.append("\n<li");
+            if (tab.isSelected()) {
+                pane.append(" class=\"sub-nav-sel\"");
+            }
+            pane.append(">");
+            pane.append("<a href=\"").append(link).append("\">").append(replaceBlanks(title)).append("</a></li>\n");
+        }
+        if (tabPane.getStyle().equals("menu")) {
+            String selected = "nav-nonsel";
+            if (tab.isSelected()) {
+                selected = "nav-sel";
+            }
+            pane.append("<li class=\"").append(selected).append("\">");
+            pane.append("<a href=\"").append(link).append("\"><span>").append(replaceBlanks(title)).append("</span></a></li>");
+        }
+        return pane;
+    }
+
+    public StringBuffer doEndBorder(GridSphereEvent event, PortletComponent comp) {
+        StringBuffer result = new StringBuffer();
+        PortletTabbedPane pane = (PortletTabbedPane) comp;
+        if (pane.getStyle().equalsIgnoreCase("sub-menu")) {
+            result.append(TAB_END_SUBMENU);
+            result.append("\n</div> <!--  END LAYOUT NAVIGATION -->\n<div id=\"gridsphere-layout-body\"> <!-- start the main portlets -->\n");
+        }
+        if (pane.getStyle().equals("menu")) {
+            result.append(TAB_END_MENU);
+        }
+        return result;
+    }
+
+    public StringBuffer doEnd(GridSphereEvent event, PortletComponent comp) {
+        StringBuffer buffer = new StringBuffer();
+        PortletTabbedPane pane = (PortletTabbedPane) comp;
+        if (pane.getStyle().equalsIgnoreCase("menu")) {
+            buffer.append("\n</div> <!-- END gridsphere-layout-body -->\n");
+        }
+        return buffer;
+    }
+}
---
+++ src/org/gridlab/gridsphere/layout/view/brush/TableLayout.java
@@ -0,0 +1,135 @@
+/*
+ * @author <a href="mailto:novotny@aei.mpg.de">Jason Novotny</a>
+ * @author <a href="mailto:oliver.wehrens@aei.mpg.de">Oliver Wehrens</a>
+ * @version $Id: TableLayout.java 4496 2006-02-08 20:27:04Z wehrens $
+ */
+package org.gridlab.gridsphere.layout.view.brush;
+
+import org.gridlab.gridsphere.layout.PortletComponent;
+import org.gridlab.gridsphere.layout.PortletFrameLayout;
+import org.gridlab.gridsphere.layout.PortletRowLayout;
+import org.gridlab.gridsphere.layout.PortletTableLayout;
+import org.gridlab.gridsphere.layout.view.BaseRender;
+import org.gridlab.gridsphere.layout.view.TableLayoutView;
+import org.gridlab.gridsphere.portlet.PortletRequest;
+import org.gridlab.gridsphere.portlet.PortletResponse;
+import org.gridlab.gridsphere.portlet.PortletURI;
+import org.gridlab.gridsphere.portlet.impl.SportletProperties;
+import org.gridlab.gridsphere.portletcontainer.GridSphereEvent;
+
+import java.util.*;
+
+public class TableLayout extends BaseRender implements TableLayoutView {
+
+    protected static final StringBuffer START_MAXIMIZED =
+            // class "window-maximized" added
+            new StringBuffer("\n<!-- START MAXIMIZED MODERN TABLE COMP --><div class=\"window-maximized\">");
+
+    protected static final StringBuffer END_MAXIMIZED =
+            new StringBuffer("\n</div><!-- END MAXIMIZED MODERN TABLE COMP -->\n");
+
+    protected static final StringBuffer END_BORDER = new StringBuffer("\n");// unuseful end-border DIV removed
+
+    protected static final StringBuffer END_TABLE = new StringBuffer("</div><!-- END MODERN TABLE -->\n");
+
+    /**
+     * Constructs an instance of PortletTableLayout
+     */
+    public TableLayout() {
+    }
+
+
+    public StringBuffer doEnd(GridSphereEvent event, PortletComponent comp) {
+        return new StringBuffer();
+    }
+
+    public StringBuffer doStartBorder(GridSphereEvent event, PortletComponent comp) {
+        StringBuffer table = new StringBuffer();
+        table.append("\n<!--  _____________________________________ table layout start -->\n");
+        return table;
+    }
+
+    public StringBuffer doEndBorder(GridSphereEvent event, PortletComponent comp) {
+        return new StringBuffer("\n<!-- _____________________________________ table layout end -->\n");
+    }
+
+
+    public StringBuffer doStartMaximizedComponent(GridSphereEvent event, PortletTableLayout tableLayout) {
+        return START_MAXIMIZED;
+    }
+
+    public StringBuffer doEndMaximizedComponent(GridSphereEvent event, PortletTableLayout tableLayout) {
+        return END_MAXIMIZED;
+    }
+
+    public StringBuffer doRenderUserSelects(GridSphereEvent event, PortletTableLayout tableLayout) {
+        event.getPortletRequest().setAttribute(SportletProperties.COMPONENT_ID, tableLayout.getLabel());
+        StringBuffer table = new StringBuffer();
+        table.append("<div class=\"row\">");
+        List components = tableLayout.getPortletComponents();
+        if (!components.isEmpty()) {
+            Object o = components.get(0);
+            if (o instanceof PortletFrameLayout) {
+                PortletFrameLayout layout = (PortletRowLayout) o;
+                List cols = layout.getPortletComponents();
+
+                for (int j = 0; j < cols.size(); j++) {
+                    o = cols.get(j);
+                    if (o instanceof PortletFrameLayout) {
+                        PortletFrameLayout col = (PortletFrameLayout) o;
+
+                        table.append("<div class=\"column\"");
+                        if (!col.getWidth().equals("")) {
+                            table.append(" style=\"width: ").append(col.getWidth()).append("\"");
+                        }
+                        table.append(">");
+                        // render add portlets listbox
+                        Map availPortlets = tableLayout.getAvailablePortletsToAdd(event);
+                        table.append(renderAddPortlets(event, j, availPortlets));
+
+                        table.append("</div>");
+                    }
+                }
+            }
+        }
+        table.append("</div>");
+        return table;
+    }
+
+    public StringBuffer renderAddPortlets(GridSphereEvent event, int col, Map availPortlets) {
+        StringBuffer table = new StringBuffer();
+
+        PortletRequest req = event.getPortletRequest();
+        PortletResponse res = event.getPortletResponse();
+        Locale locale = req.getLocale();
+        PortletURI uri = res.createURI();
+        uri.addAction(PortletTableLayout.PORTLET_ADD_ACTION);
+
+        table.append("<form action=\"").append(uri.toString()).append("\"  method=\"post\" name=\"addform\"><p>");
+        table.append("<input type=\"hidden\" name=\"" + PortletTableLayout.PORTLET_COL + "\" value=\"").append(col).append("\"/>");
+
+        ResourceBundle bundle = ResourceBundle.getBundle("gridsphere.resources.Portlet", locale);
+        String addLabel = bundle.getString("ADDPORTLETS");
+        String addButton = bundle.getString("ADD");
+        String noPortletsMsg = bundle.getString("NOPORTLETS");
+
+        table.append("<b>").append(addLabel).append("</b>&nbsp;&nbsp;&nbsp;");
+        table.append("<select name=\"" + PortletTableLayout.PORTLET_ADD_ACTION + "\">");
+
+        if (availPortlets.isEmpty()) {
+            table.append("<option value=\"" + PortletTableLayout.PORTLET_NO_ACTION + "\">").append(noPortletsMsg).append("</option>");
+        }
+
+        Iterator it = availPortlets.keySet().iterator();
+        while (it.hasNext()) {
+            String pid = (String) it.next();
+            String dispName = (String) availPortlets.get(pid);
+            table.append("<option value=\"").append(pid).append("\">").append(dispName).append("</option>");
+        }
+
+        table.append("</select>");
+        table.append("&nbsp;&nbsp;<input type=\"submit\" name=\"gs_action=addportlet\" value=\"").append(addButton).append("\">");
+        table.append("</p></form>");
+        return table;
+    }
+}
