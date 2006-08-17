b532b9e813bb34efdbfc259fc5642c855e4680bd
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2006-08-17 18:38:19 +0000

    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@5039 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

---
+++ src/org/gridlab/gridsphere/portlets/core/logout/LogoutPortlet.java
@@ -0,0 +1,26 @@
+/*
+ * @author <a href="mailto:novotny@aei.mpg.de">Jason Novotny</a>
+ * @version $Id: LogoutPortlet.java 4496 2006-02-08 20:27:04Z wehrens $
+ */
+package org.gridlab.gridsphere.portlets.core.logout;
+
+import org.gridlab.gridsphere.portlet.*;
+import org.gridlab.gridsphere.portlet.impl.AbstractPortlet;
+
+import java.io.IOException;
+import java.util.Locale;
+
+
+public class LogoutPortlet extends AbstractPortlet {
+
+    public void doView(PortletRequest request, PortletResponse response) throws PortletException, IOException {
+        Client client = request.getClient();
+        String title;
+        Locale locale = request.getLocale();
+        title = getPortletSettings().getTitle(locale, client);
+        request.setAttribute("GRIDSPHERE_LOGOUT_LABEL", title);
+        request.setAttribute("username", request.getUser().getFullName());
+        getPortletConfig().getContext().include("/jsp/login/logout.jsp", request, response);
+    }
+
+}
