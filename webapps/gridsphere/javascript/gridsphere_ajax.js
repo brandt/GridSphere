efdac918fc3b2a1515d11986406f28cc788b39eb
novotny <novotny@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2006-11-30 18:18:05 +0000

    my work on ajax portlets
    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/trunk@5779 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

--- webapps/gridsphere/javascript/gridsphere_ajax.js
+++ webapps/gridsphere/javascript/gridsphere_ajax.js
@@ -82,3 +82,86 @@ var GridSphereAjaxHandler = {
     }
 
 };
+
+var GridSphereAjaxHandler2 = {
+
+    handleSuccess:function(o) {
+        var name = o.argument[0];
+        //alert(name);
+        if(o.responseText != undefined) {
+            //alert(o.responseText);
+
+            // check to see if entire HTML has been transferred back
+
+            var fragment = o.responseText;
+
+            // check for a redirect!
+            if (fragment.substring(0,4) == "http") {
+                window.location = fragment;
+                return;
+            }
+
+            // create a temporary div element to store the responseText..
+            var tmpDiv = document.createElement("div");
+            // ... and store the responseText inside it
+
+            tmpDiv.innerHTML = fragment;
+
+            var frame = YAHOO.util.Dom.getElementsByClassName('gridsphere-window-content', 'div', tmpDiv);
+            var titlebar = YAHOO.util.Dom.getElementsByClassName('gridsphere-window-title', 'div', tmpDiv);
+            var panel = document.getElementById(name);
+            var title = YAHOO.util.Dom.getElementsByClassName('hd', 'div', panel);
+            var body = YAHOO.util.Dom.getElementsByClassName('bd', 'div', panel);
+            body[0].innerHTML = frame[0].innerHTML;
+            title[0].innerHTML = titlebar[0].innerHTML; // + "<a style=\"position:absolute; top:5px; right:4px; z-index:6; height:12px; width:12px; margin:0px; padding:0px; background-repeat:no-repeat; cursor:pointer; visibility:inherit;\" href=\"#\" onclick=\"YAHOO." + name + ".panel.hide();\">close</a>";
+
+            //alert(o.responseText);
+            // create a collection of all the div elements returned
+            //var tempPageElements = serverResponse.getElementsByTagName("div");
+            // replace existing component with new one
+            //document.getElementById(name).innerHTML = tempPageElements[0].innerHTML;
+            //window.eval("YAHOO." + name + ".panel.show()");
+        }
+    },
+
+    handleFailure:function(o) {
+        var cid = o.argument[0];
+        if(o.responseText != undefined) {
+            // replace existing component with new one           
+            //alert("failure! " + o.statusText);
+            //alert(o.getResponseHeader());
+
+            document.getElementById(name).innerHTML = "Unable to retrieve portlet fragment!!! ";
+        }
+    },
+
+    startRequest:function(portlet, name, action) {
+
+        var pageElements = document.getElementById(name);
+        var formElements = pageElements.getElementsByTagName("form");
+
+        YAHOO.util.Connect.setForm(formElements[0]);
+
+        var extra =  YAHOO.util.Connect._sFormData;
+       
+        if (action != null) {
+            extra += action;
+        }
+        if (extra == null) {
+            extra = "";
+        } else {
+            extra = "&" + extra;
+        }
+
+        var sUrl = "/gridsphere/gs" + "?ajax=true&compname=" + name + "&portlet=" + encodeURIComponent(portlet) + extra;
+
+        var postData = null;
+
+        var transaction = YAHOO.util.Connect.asyncRequest('POST', sUrl,
+            {success:this.handleSuccess, failure:this.handleFailure, argument:[ name ], scope:this},
+                postData);
+        //Abort the transaction if it isn't completed in ten seconds.
+        //setTimeout("YAHOO.util.Connect.abort(transaction)",10000);
+    }
+
+};
--- webapps/gridsphere/javascript/yahoo/ResizePanel.js
+++ webapps/gridsphere/javascript/yahoo/ResizePanel.js
@@ -1,105 +1,105 @@
 YAHOO.widget.ResizePanel = function(el, userConfig) {
-	if (arguments.length > 0) {
-		YAHOO.widget.ResizePanel.superclass.constructor.call(this, el, userConfig);
-	}
+    if (arguments.length > 0) {
+        YAHOO.widget.ResizePanel.superclass.constructor.call(this, el, userConfig);
+    }
 }
 
 YAHOO.extend(YAHOO.widget.ResizePanel, YAHOO.widget.Panel);
 
 YAHOO.widget.ResizePanel.prototype.init = function(el, userConfig) {
-	YAHOO.widget.ResizePanel.superclass.init.call(this, el/*, userConfig*/);  // Note that we don't pass the user config in here yet because we only want it executed once, at the lowest subclass level
-	
-	this.beforeInitEvent.fire(YAHOO.widget.ResizePanel);
+    YAHOO.widget.ResizePanel.superclass.init.call(this, el/*, userConfig*/);  // Note that we don't pass the user config in here yet because we only want it executed once, at the lowest subclass level
 
-	this.resizeHandle = document.createElement("DIV");
-	this.resizeHandle.id = this.id + "_r";
-	
-	this.resizeHandle.style.position = "absolute";
-	this.resizeHandle.style.width = "25px";
-	this.resizeHandle.style.height = "4px";
-	this.resizeHandle.style.right = "0";
-	this.resizeHandle.style.bottom = "0";
-	this.resizeHandle.style.padding = "0";
-	this.resizeHandle.style.margin = "0";
-	this.resizeHandle.style.zIndex = "1";
-	
-	this.resizeHandle.style.backgroundColor = "#666";
-	this.resizeHandle.style.cursor = "se-resize";
-	this.resizeHandle.style.fontSize = "2px";
+    this.beforeInitEvent.fire(YAHOO.widget.ResizePanel);
 
-	this.beforeRenderEvent.subscribe(function() {
-			if (! this.footer) {
-				this.setFooter("");
-			}
-		}, 
-		this, true
-	);
-	this.renderEvent.subscribe(function() {
-					var me = this;
-					
-					me.innerElement.appendChild(me.resizeHandle);
+    this.resizeHandle = document.createElement("DIV");
+    this.resizeHandle.id = this.id + "_r";
 
-					this.ddResize = new YAHOO.util.DragDrop(this.resizeHandle.id, this.id);
-					this.ddResize.setHandleElId(this.resizeHandle.id);
+    this.resizeHandle.style.position = "absolute";
+    this.resizeHandle.style.width = "25px";
+    this.resizeHandle.style.height = "4px";
+    this.resizeHandle.style.right = "0";
+    this.resizeHandle.style.bottom = "0";
+    this.resizeHandle.style.padding = "0";
+    this.resizeHandle.style.margin = "0";
+    this.resizeHandle.style.zIndex = "1";
 
-					var headerHeight = me.header.offsetHeight;
+    this.resizeHandle.style.backgroundColor = "#666";
+    this.resizeHandle.style.cursor = "se-resize";
+    this.resizeHandle.style.fontSize = "2px";
 
-					this.ddResize.onMouseDown = function(e) {
+    this.beforeRenderEvent.subscribe(function() {
+        if (! this.footer) {
+            this.setFooter("");
+        }
+    },
+            this, true
+            );
+    this.renderEvent.subscribe(function() {
+        var me = this;
 
-						this.startWidth = me.innerElement.offsetWidth;
-						this.startHeight = me.innerElement.offsetHeight;
-						
-						me.cfg.setProperty("width", this.startWidth + "px");
-						me.cfg.setProperty("height", this.startHeight + "px");
+        me.innerElement.appendChild(me.resizeHandle);
 
-						this.startPos = [YAHOO.util.Event.getPageX(e),
-										 YAHOO.util.Event.getPageY(e)];
+        this.ddResize = new YAHOO.util.DragDrop(this.resizeHandle.id, this.id);
+        this.ddResize.setHandleElId(this.resizeHandle.id);
 
-						me.innerElement.style.overflow = "hidden";
-						me.body.style.overflow = "auto";
-					}
-					
-					this.ddResize.onDrag = function(e) {
-						var newPos = [YAHOO.util.Event.getPageX(e),
-									  YAHOO.util.Event.getPageY(e)];
-						
-						var offsetX = newPos[0] - this.startPos[0];
-						var offsetY = newPos[1] - this.startPos[1];
-				
-						var newWidth = Math.max(this.startWidth + offsetX, 10);
-						var newHeight = Math.max(this.startHeight + offsetY, 10);
+        var headerHeight = me.header.offsetHeight;
 
-						me.cfg.setProperty("width", newWidth + "px");
-						me.cfg.setProperty("height", newHeight + "px");
+        this.ddResize.onMouseDown = function(e) {
 
-						var bodyHeight = (newHeight - 5 - me.footer.offsetHeight - me.header.offsetHeight - 3);
-						if (bodyHeight < 0) {
-							bodyHeight = 0;
-						}
+            this.startWidth = me.innerElement.offsetWidth;
+            this.startHeight = me.innerElement.offsetHeight;
 
-						me.body.style.height =  bodyHeight + "px";
+            me.cfg.setProperty("width", this.startWidth + "px");
+            me.cfg.setProperty("height", this.startHeight + "px");
 
-						var innerHeight = me.innerElement.offsetHeight;
-						var innerWidth = me.innerElement.offsetWidth;
+            this.startPos = [YAHOO.util.Event.getPageX(e),
+                    YAHOO.util.Event.getPageY(e)];
 
-						if (innerHeight < headerHeight) {
-							me.innerElement.style.height = headerHeight + "px";
-						}
+            me.innerElement.style.overflow = "hidden";
+            me.body.style.overflow = "auto";
+        }
 
-						if (innerWidth < 20) {
-							me.innerElement.style.width = "20px";
-						}
-					}
+        this.ddResize.onDrag = function(e) {
+            var newPos = [YAHOO.util.Event.getPageX(e),
+                    YAHOO.util.Event.getPageY(e)];
 
-				}, this, true);
+            var offsetX = newPos[0] - this.startPos[0];
+            var offsetY = newPos[1] - this.startPos[1];
 
-	if (userConfig) {
-		this.cfg.applyConfig(userConfig, true);
-	}
+            var newWidth = Math.max(this.startWidth + offsetX, 10);
+            var newHeight = Math.max(this.startHeight + offsetY, 10);
 
-	this.initEvent.fire(YAHOO.widget.ResizePanel);
+            me.cfg.setProperty("width", newWidth + "px");
+            me.cfg.setProperty("height", newHeight + "px");
+
+            var bodyHeight = (newHeight - 5 - me.footer.offsetHeight - me.header.offsetHeight - 3);
+            if (bodyHeight < 0) {
+                bodyHeight = 0;
+            }
+
+            me.body.style.height =  bodyHeight + "px";
+
+            var innerHeight = me.innerElement.offsetHeight;
+            var innerWidth = me.innerElement.offsetWidth;
+
+            if (innerHeight < headerHeight) {
+                me.innerElement.style.height = headerHeight + "px";
+            }
+
+            if (innerWidth < 20) {
+                me.innerElement.style.width = "20px";
+            }
+        }
+
+    }, this, true);
+
+    if (userConfig) {
+        this.cfg.applyConfig(userConfig, true);
+    }
+
+    this.initEvent.fire(YAHOO.widget.ResizePanel);
 }
 
 YAHOO.widget.ResizePanel.prototype.toString = function() {
-	return "ResizePanel " + this.id;
+    return "ResizePanel " + this.id;
 }
