4cd7b71e52f82dfde78a09a81b0b7dd23fdf65b3
docentt <docentt@9c99c85f-4d0c-0410-8460-a9a1c48a3a7f>
2007-08-07 10:07:04 +0000

    git-svn-id: http://svn.aei.mpg.de/eScience/GridSphere/branches/GRIDSPHERE_2_2_X_PSNC@6331 9c99c85f-4d0c-0410-8460-a9a1c48a3a7f

--- src/org/gridsphere/services/core/security/auth/modules/impl/BaseLateUserRetrievalAuthModule.java
+++ src/org/gridsphere/services/core/security/auth/modules/impl/BaseLateUserRetrievalAuthModule.java
@@ -4,7 +4,12 @@ import org.gridlab.gridsphere.services.core.security.auth.modules.impl.BaseAuthM
 import org.gridlab.gridsphere.services.core.security.auth.modules.impl.descriptor.AuthModuleDefinition;
 import org.gridlab.gridsphere.services.core.security.auth.AuthenticationException;
 import org.gridlab.gridsphere.portlet.User;
+import org.gridlab.gridsphere.portlet.PortletRequest;
 import org.gridsphere.services.core.security.auth.modules.LateUserRetrievalAuthModule;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Enumeration;
 
 /**
  * @author <a href="mailto:docentt@man.poznan.pl">Tomasz Kuczynski</a>, PSNC
@@ -21,5 +26,16 @@ public abstract class BaseLateUserRetrievalAuthModule extends BaseAuthModule imp
 
     public void throwAuthenticationException(String message) throws AuthenticationException {
         throw new AuthenticationException(message);
+    }
+
+    public Map getRequestAttributes(AuthenticationParameters authenticationParameters){
+        HashMap tore = new HashMap();
+        PortletRequest portletRequest = (PortletRequest) authenticationParameters.getRequest();
+        Enumeration attributeNames = portletRequest.getAttributeNames();
+        while (attributeNames.hasMoreElements()) {
+            String attributeName = (String) attributeNames.nextElement();
+            tore.put(attributeName, portletRequest.getAttribute(attributeName));
+        }
+        return tore;
     }
 }
